051f63f135699742a9829ac3b65239afbf47fd77
Will Hardiman <bill.hardiman1995@gmail.com>
2020-03-23 12:56:12 +0000

    updated name comparisons to use filename only

--- helpers/compare_info_meta_imstack.m
+++ helpers/compare_info_meta_imstack.m
@@ -1,11 +1,15 @@
-function compare_info_meta_imstack(info, meta, Imstack)
+function compare_info_meta_imstack(Info, Meta, Imstack)
+SplitInfo = strsplit(Info,'/');
+SplitMeta = strsplit(Meta, '/');
+SplitImstack = strsplit(Imstack,{'/',' '});
+
 if isempty(whos('Imstack'))
     error('You need an Imstack')
 elseif isempty(whos('info')) || isempty(whos('meta'))
     error('You need info and meta')
-elseif ~strcmp(info(1).filepath,meta.filepath) || ~strncmp(meta.filepath, Imstack{1}{1,2}, length(meta.filepath))
+elseif ~strcmp(SplitInfo(1).filepath,SplitMeta.filepath) || ~strncmp(SplitMeta.filepath, SplitImstack{1}{1,2}, length(SplitMeta.filepath))
     fprintf('Info for dataset %s\n Meta for dataset %s\n Imstack for dataset %s\n',...
-        info(1).filepath, meta.filepath, Imstack{1}{1,2}(1:length(meta.filepath)))
+        SplitInfo(1).filepath, SplitMeta.filepath, SplitImstack{1}{1,2}(1:length(SplitMeta.filepath)))
     error('info, meta, and Imstack need to be for the same dataset')
 end
 end
