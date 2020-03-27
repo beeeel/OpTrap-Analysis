d78d0cf3d0c89d076e9ec01c351d805448a6adac
will hardiman <bill.hardiman1995@gmail.com>
2020-03-27 09:32:19 +0000

    Separated loading of Imstack etc from seg_quality

--- helpers/seg_quality.m
+++ helpers/seg_quality.m
@@ -5,42 +5,9 @@
 %% Load data
 CellType = 'LS174T';
 Set = 'normoxia';
-Num = '11';
-datadir = '/home/will/Documents/data/OpTrap/';
-infosdir = [datadir 'infos/'];
-if isempty(whos('Imstack')); Imstack = {{0,''}}; end % Create an empty
+Num = '14';
 
-if strcmp(CellType,'hela')
-    load([infosdir 'info-hela_ctrl_s_020_tr_70_' Num '.mat'])
-    imfile = [datadir '0610/Deformation/hela_ctrl_s_020_tr_70_' Num '_MMStack.ome.tif']
-    if ~strcmp(imfile, Imstack{1}{1,2}(1:length(imfile)))
-        Imstack = load_imstack('0610/Deformation','ctrl','020','70',0,Num);
-    end
-elseif strcmp(Set,'Jenna')
-    error('you need to update this branch of the load operation')
-    load(strcat('/home/ppxwh2/Documents/data/OpTrap/infos/info_Jenna_test_no_erode',Num,'.mat'))
-    imfile = ['/home/ppxwh2/Documents/data/OpTrap/1119_Jenna/191119_thp1_ctrl_s_010_tr_50_',Num,'_MMStack.ome.tif']
-    if ~strcmp(imfile, Imstack{1}{1,2}(1:length(imfile)))
-        Imstack = bfopen(imfile);
-    end
-elseif strcmp(CellType,'HL60')
-    if strcmp(Set,'with_drugs')
-        matName = ['_190717_HL60_' Num '_0.020mm-1_1'];
-    elseif strcmp(Set,'normoxia')
-        matName = ['_100717_' Num '_' CellType '_1']; 
-    end
-    load(['/home/ppxwh2/Documents/data/OpTrap/infos/info_' CellType '_' Set matName '.mat'])
-    imfile = ['/home/ppxwh2/Documents/data/OpTrap/2017_10_movies-from-aishah/'...
-        CellType '/' CellType '_' Set '/' matName(2:end) '.avi']
-    if ~strcmp(imfile, Imstack{1}{1,2}(1:length(imfile)))
-        Imstack = avi_to_imstack(imfile)
-    end
-elseif strcmp(CellType,'LS174T')
-    imfile = ['200717_' Num '_LS174T_' Set '_1.avi'];
-    if strcmp(Set,'hypoxia'); imfile(2) = '1'; end
-    load([infosdir 'info_seg_LS174T_' Set '_' imfile(1:end-4) '.mat'])
-    Imstack = avi_to_imstack([datadir '2017_10_movies-from-aishah/LS174T/' imfile]);
-end
+[Imstack, info, meta] = LoadImstackInfoMeta(CellType,Set,Num);
 
 %% Segmentation video
 % This can also save a gif or an avi movie
