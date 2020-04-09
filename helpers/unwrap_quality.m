243153b520d3a184808df2106f32ec4275156ced
will hardiman <bill.hardiman1995@gmail.com>
2020-04-09 17:42:16 +0100

    Finished(?) working on viewing fits and CI.

--- helpers/unwrap_quality.m
+++ helpers/unwrap_quality.m
@@ -2,15 +2,15 @@
 % Show fit on cell and fit from different frame on cell
 
 % Load
-CellType = 'LS174T';
-Set = 'normoxia';
-Num = '11';
+CellType = 'HL60';
+Set = 'with_drugs';
+Num = '19';
 
 % Display options
-Frs = [1, 736]; % Which frames
+Frs = [1, 1000]; % Which frames
 
 % Fontsizes
-FSizes.Ttl1 = 16; % Titles
+FSizes.Ttl1 = 20; % Titles
 FSizes.Ttl2 = 14;
 FSizes.XL1 = 14; % YLabels
 FSizes.XL2 = 10;
@@ -23,38 +23,24 @@ if length(args)>1
     [Unwrapped, Ia, FitEqn] = args{:};
 end
 
-%% Show overlaid images and fits
-fh = figure(37);
-colormap gray
-for fr = Frs
-    subplot(2,length(Frs), find(Frs==fr))
-    imagesc(Imstack{1}{fr,1})
-    axis image off, hold on
-    PlotEllipseOverlay(2 * info(fr).uMajorAxisLength, 2*info(fr).uMinorAxisLength,...
-        info(fr).uOrientation, info(fr).mCentres + info(fr).uOffset(2:3))
-    title(['Frame ' num2str(fr) ' with fit'])
-    
-    subplot(2, length(Frs), length(Frs) + find(Frs==fr))
-    imagesc(Imstack{1}{fr,1})
-    axis image off, hold on
-    PlotEllipseOverlay(2 * info(Frs(Frs~=fr)).uMajorAxisLength, 2*info(Frs(Frs~=fr)).uMinorAxisLength,...
-        info(Frs(Frs~=fr)).uOrientation, info(Frs(Frs~=fr)).mCentres + info(Frs(Frs~=fr)).uOffset(2:3))
-    title(['Frame ' num2str(fr) ' with fit from frame ' num2str(Frs(Frs~=fr))])
-end
+%% Show overlaid images and fits with confidence interval
+% Fits are rotated and translated to orientation and centre of cell.
+% Confidence interval from standard deviation of initial deformation.
+SaveFig = true;
+SavePng = true;
 
-%% Same but with nice title
 Ds = [info.uTaylorParameter];
 DErrs = repmat(std(Ds(1:100)),1,length(info));
 as = [info.uMajorAxisLength];
 bs = [info.uMinorAxisLength];
 
-aErrs = sqrt(2) * (as.^2 .* (DErrs./Ds).^2 .* (1 - Ds) ./ (as + bs).^2);
-bErrs = sqrt(2) * (bs.^2 .* (DErrs./Ds).^2 .* (1 - Ds) ./ (as + bs).^2);
-
-Titles = {['Frame ' num2str(Frs(1)) ' with fit from frame ' num2str(Frs(1))]; 
-    ['Frame ' num2str(Frs(2)) ' with fit from frame ' num2str(Frs(1))]; 
-    ['Frame ' num2str(Frs(1)) ' with fit from frame ' num2str(Frs(2))]; 
-    ['Frame ' num2str(Frs(2)) ' with fit from frame ' num2str(Frs(2))]};
+aErrs = sqrt(2 * (as.^2 .* (DErrs./Ds).^2 .* (1 - Ds) ./ (as + bs).^2));
+bErrs = sqrt(2 * (bs.^2 .* (DErrs./Ds).^2 .* (1 + 3 * Ds) ./ (as + bs).^2));
+FSpec = 'D = %.4f ± %.4f';
+Titles = {{['Frame ' num2str(Frs(1)) ' with fit from frame ' num2str(Frs(1))], sprintf(FSpec ,info(Frs(1)).uTaylorParameter, DErrs(1))}; 
+    {['Frame ' num2str(Frs(1)) ' with fit from frame ' num2str(Frs(2))], sprintf(FSpec ,info(Frs(2)).uTaylorParameter, DErrs(1))}; 
+    {['Frame ' num2str(Frs(2)) ' with fit from frame ' num2str(Frs(1))], sprintf(FSpec ,info(Frs(1)).uTaylorParameter, DErrs(1))}; 
+    {['Frame ' num2str(Frs(2)) ' with fit from frame ' num2str(Frs(2))], sprintf(FSpec ,info(Frs(2)).uTaylorParameter, DErrs(1))}};
 Frames = [Frs; Frs];
 M = 11;
 N = 11;
@@ -67,26 +53,30 @@ ax = gca;
 ax.Color = ax.Parent.Color;
 ax.XColor = ax.Parent.Color;
 ax.YColor = ax.Parent.Color;
-title({[CellType ' ' Set ' ' Num ' example fits with CI'], 'Showing fitted axis + confidence interval with errors from variance of '},'FontSize',FSizes.Ttl1)
+title({[CellType ' ' Set ' ' Num ' example fits with CI'], 'Showing fitted axis + confidence interval with errors from','standard deviation of initial deformation'},...
+    'FontSize',FSizes.Ttl1)
 
 for n = 0:3
-    fr1 = Frs(mod(n,2)+1); 
-    fr2 = Frs(ceil((n+1)/2));
+    fr2 = Frs(mod(n,2)+1); 
+    fr1 = Frs(ceil((n+1)/2));
     % The workhorse of the loop - the list of subplots covered
     V = floor(n/2) * N*(M-1)/2 + mod(n,2) * (N+1)/2 + (1:(N-1)/2) + (2*N:N:N*(-3+M)/2)';
     subplot(M,N,reshape(V,1,[]))
     imagesc(Imstack{1}{fr1,1})
     axis image off, hold on
-    PlotEllipseOverlay(2 * info(fr1).uMajorAxisLength, 2*info(fr1).uMinorAxisLength,...
-        info(fr1).uOrientation, info(fr1).mCentres + info(fr1).uOffset(2:3))
+    PlotEllipseOverlay(2 * info(fr2).uMajorAxisLength, ...
+        2*info(fr2).uMinorAxisLength,...
+        info(fr1).uOrientation, info(fr1).mCentres + info(fr1).uOffset(2:3),'k','LineWidth',3)
     PlotEllipseOverlay(2 * (info(fr2).uMajorAxisLength + aErrs(fr2)), ...
         2 * (info(fr2).uMinorAxisLength - bErrs(fr2) ),...
-        info(fr2).uOrientation, info(fr1).mCentres + info(fr1).uOffset(2:3),'r:')
+        info(fr1).uOrientation, info(fr1).mCentres + info(fr1).uOffset(2:3),'r:','LineWidth',2)
     PlotEllipseOverlay(2 * (info(fr2).uMajorAxisLength - aErrs(fr2)), ...
         2 * (info(fr2).uMinorAxisLength + bErrs(fr2) ),...
-        info(fr2).uOrientation, info(fr1).mCentres + info(fr1).uOffset(2:3),'b:')
+        info(fr1).uOrientation, info(fr1).mCentres + info(fr1).uOffset(2:3),'b--','LineWidth',2)
     title(Titles{n+1},'FontSize',FSizes.Ttl2)
-end 
+end
+
+SaveFigPng(['fits_CI_std_D_' strjoin({CellType,Set,Num},'_')],'EQ',true,true)
 
 %%
 function [Out] = N_TidyLoader(CellType, Set, Num)
