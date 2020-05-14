0a667cbce71e7e66aff31aba24e73798074259ba
will hardiman <bill.hardiman1995@gmail.com>
2020-05-14 15:55:20 +0100

    Adapted towards goal of using 1D fitting for centering and 2D for measuring. Also replaced fiteqn with fittype to reduce calls to fittype

--- modules/unwrap_cell_v4.m
+++ modules/unwrap_cell_v4.m
@@ -54,7 +54,7 @@ function [Fits, varargout] = unwrap_cell_v4(Imstack, Centres, Radius, varargin)
 fields = {'sc_up', 'n_theta', 'n_reps', 'tol', 'inter_method', 'sc_down',...
     'centering', 'ifNaN','parallel','weighted','extrap_val'};
 defaults = {1.2, 360, 5, 0.15, 'linear', 0.5,...
-    0, 'mean',false, true, 0};
+    '1D', 'mean',false, true, 0};
 
 tic
 
@@ -64,8 +64,8 @@ Par = ParseInputs(fields, defaults, varargin{:});
 [Centres, Radius] = FixNaNs(Centres, Radius, Imstack, Par);
 
 % Get the circle equation
-if strcmp(Par.centering,'circle')
-    [CircleEqn, lb, ub, StartVal] = GetEqn('circle', Imstack, Par, Radius);
+if strcmp(Par.centering,'circle') || strcmp(Par.centering,'1D')
+    [CircleFit, lb, ub, StartVal] = GetEqn('circle', Imstack, Par, Radius);
        
     % Debugging memory usage
     Var = whos;
@@ -73,18 +73,17 @@ if strcmp(Par.centering,'circle')
     clear Var;
     
     % Perform unwrapping and fitting for off-centreness
-    [Offset, ~, ~] = UnwrapAndFit(Imstack, CircleEqn, Radius,  Centres, lb, ub, StartVal, Par);
+    [Offset, ~, ~] = UnwrapAndFit(Imstack, CircleFit, Radius,  Centres, lb, ub, StartVal, Par);
 end
 
 % Get the ellipse equation
-[FitEqn, lb, ub, StartVal] = GetEqn('ellipse', Imstack, Par, Radius);
+[EllipseFit, lb, ub, StartVal] = GetEqn('ellipse', Imstack, Par, Radius);
 
 % Perform unwrapping and fitting with updated centre locations
-switch Par.centering
-    case 'circle'
-        [Fits, Unwrapped, FitErrs] = UnwrapAndFit(Imstack, FitEqn, Offset(1,:),  Centres + Offset(4:5,:), lb, ub, StartVal, Par);
-    otherwise
-        [Fits, Unwrapped, FitErrs] = UnwrapAndFit(Imstack, FitEqn, Radius,  Centres, lb, ub, StartVal, Par);
+if ~ isempty(whos('Offset'))
+    [Fits, Unwrapped, FitErrs] = UnwrapAndFit(Imstack, EllipseFit, Offset(1,:),  Centres + Offset(4:5,:), lb, ub, StartVal, Par);
+else
+    [Fits, Unwrapped, FitErrs] = UnwrapAndFit(Imstack, EllipseFit, Radius,  Centres, lb, ub, StartVal, Par);
 end
 
 
@@ -94,17 +93,17 @@ Fits(3,:) = mod(Fits(3,:)+pi/2, pi) - pi/2;
 Fits(1:2,:) = [max(Fits(1:2,:)); min(Fits(1:2,:))];
 switch nargout
     case 2; varargout = {Unwrapped};
-    case 3; varargout = {Unwrapped, FitEqn};
-    case 4; varargout = {Unwrapped, FitEqn, []};
-    case 5; varargout = {Unwrapped, FitEqn, [], FitErrs};
-    case 6; varargout = {Unwrapped, FitEqn, [], FitErrs, Par}; % empty matrix replaced Offset
+    case 3; varargout = {Unwrapped, EllipseFit};
+    case 4; varargout = {Unwrapped, EllipseFit, []};
+    case 5; varargout = {Unwrapped, EllipseFit, [], FitErrs};
+    case 6; varargout = {Unwrapped, EllipseFit, [], FitErrs, Par}; % empty matrix replaced Offset
 end
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %% Functions start here
-    function [Fits, Unwrapped, Errs] = UnwrapAndFit(Imstack, FitEqn, Radius, Centres, lb, ub, StartVal, Par)
+    function [Fits, Unwrapped, Errs] = UnwrapAndFit(Imstack, FitType, Radius, Centres, lb, ub, StartVal, Par)
         %% Fairly self-explanatory tbh. Unwraps data using interp3, finds edges, and fits to fit equation
         % Find the fitting variables to determine size of fits array
-        FitVars = coeffnames(fittype(FitEqn,'independent',{'Theta','R'}));
+        FitVars = coeffnames(FitType);
         N_Frs = length(Imstack{1});
         
         % Preallocate
@@ -133,50 +132,51 @@ end
             Par.inter_method, Par.extrap_val));                                                                             % METHOD
         fprintf('Finished unwrapping at %gs\n',toc)
         
-        [Fits, Errs] = N_DoUnwrappedFits(Theta, Rs, Unwrapped, FitEqn, lb, ub, StartVal, Fits, Errs, N_Frs, Par);
+        [Fits, Errs] = N_DoUnwrappedFits(Theta, Rs, Unwrapped, FitType, lb, ub, StartVal, Fits, Errs, N_Frs, Par);
         
         fprintf('%s\r',repmat(' ',1,104))
         fprintf('Fitted data at %gs\n',toc)
     end
 
-    function [Fits, Errs] = N_DoUnwrappedFits(Theta, Rs, Unwrapped, FitEqn, lb, ub, StartVal, Fits, Errs, N_Frs, Par)
+    function [Fits, Errs] = N_DoUnwrappedFits(Theta, Rs, Unwrapped, ThisFit, lb, ub, StartVal, Fits, Errs, N_Frs, Par)
         %% Performs fits - parallel or non, or weighted
         % For each frame, take angles corresponding to fit points and perform
         % the fit
         disp('Starting fitting')
-        ThisFit = fittype(FitEqn,'independent',{'Theta','R'});
         FitVars = coeffnames(ThisFit);
-        if ~Par.parallel
-            if Par.weighted                
-                WFun = @(r, theta, frame) repmat(normalize(normpdf(1:max(Rs),Radius(frame), Radius(frame)/2),'scale','first')',1,length(theta));
-                for frame = 1:N_Frs
-                    % Take corresponding angles, repeat them and unwrap
-                    Weights = WFun(Rs, Theta, frame);
-                    [ThOut, rOut, IOut, WOut] = prepareSurfaceData(Theta, Rs, Unwrapped(:,:,frame), Weights);
-                    fitobj = fit([ThOut, rOut], IOut,ThisFit,'Upper',ub,'Lower',lb,'Start',StartVal(frame,:),'Weights',WOut);
-                    for VarN = 1:length(FitVars) % Extract fitted values from cfit object
-                        Fits(VarN, frame) = fitobj.(FitVars{VarN});
+        switch length(indepnames(ThisFit))
+            case 2
+                if ~Par.parallel
+                    if Par.weighted
+                        WFun = @(r, theta, frame) repmat(normalize(normpdf(1:max(Rs),Radius(frame), Radius(frame)/2),'scale','first')',1,length(theta));
+                        for frame = 1:N_Frs
+                            % Take corresponding angles, repeat them and unwrap
+                            Weights = WFun(Rs, Theta, frame);
+                            [ThOut, rOut, IOut, WOut] = prepareSurfaceData(Theta, Rs, Unwrapped(:,:,frame), Weights);
+                            fitobj = fit([ThOut, rOut], IOut,ThisFit,'Upper',ub,'Lower',lb,'Start',StartVal(frame,:),'Weights',WOut);
+                            for VarN = 1:length(FitVars) % Extract fitted values from cfit object
+                                Fits(VarN, frame) = fitobj.(FitVars{VarN});
+                            end
+                            ConfInt = confint(fitobj);
+                            Errs(:,frame) = (ConfInt(2,:) - ConfInt(1,:))/2;
+                            ProgressBar(frame./N_Frs)
+                        end
+                    else
+                        for frame = 1:N_Frs
+                            % Take corresponding angles, repeat them and unwrap
+                            [ThOut, rOut, IOut] = prepareSurfaceData(Theta, Rs, Unwrapped(:,:,frame));
+                            fitobj = fit([ThOut, rOut], IOut,ThisFit,'Upper',ub,'Lower',lb,'Start',StartVal(frame,:));
+                            for VarN = 1:length(FitVars) % Extract fitted values from cfit object
+                                Fits(VarN, frame) = fitobj.(FitVars{VarN});
+                            end
+                            ConfInt = confint(fitobj);
+                            Errs(:,frame) = (ConfInt(2,:) - ConfInt(1,:))/2;
+                            ProgressBar(frame./N_Frs)
+                        end
                     end
-                    ConfInt = confint(fitobj);
-                    Errs(:,frame) = (ConfInt(2,:) - ConfInt(1,:))/2;
-                    ProgressBar(frame./N_Frs)
-                end
-            else
-                for frame = 1:N_Frs
-                    % Take corresponding angles, repeat them and unwrap
-                    [ThOut, rOut, IOut] = prepareSurfaceData(Theta, Rs, Unwrapped(:,:,frame));
-                    fitobj = fit([ThOut, rOut], IOut,ThisFit,'Upper',ub,'Lower',lb,'Start',StartVal(frame,:));
-                    for VarN = 1:length(FitVars) % Extract fitted values from cfit object
-                        Fits(VarN, frame) = fitobj.(FitVars{VarN});
-                    end
-                    ConfInt = confint(fitobj);
-                    Errs(:,frame) = (ConfInt(2,:) - ConfInt(1,:))/2;
-                    ProgressBar(frame./N_Frs)
-                end
-            end
-        else
-            error('Parallel not programmed yet')
-            %{
+                else
+                    error('Parallel not programmed yet')
+                    %{
     if Par.weighted
         warning('Weighted argument ignored because running parallel. Write some more code!')
     end
@@ -200,15 +200,137 @@ end
         ConfInt = confint(fitobj);
         Errs(:,frame) = (ConfInt(2,:) - ConfInt(1,:))/2;
     end
-            %}
+                    %}
+                end
+            case 1
+                [Ia, idxa] = FindMaxes(Unwrapped, Radius, Par);
+                if ~Par.parallel
+                    if Par.weighted
+                        if length(FitVars) == 3
+                            WFitEqn = @(val, x) FitEqn(val(1),val(2),val(3),x);
+                        else
+                            WFitEqn = @(val, x) FitEqn(val(1),val(2),val(3),val(4),val(5),x);
+                        end
+                        
+                        for frame = 1:N_Frs
+                            th_fit = repmat(Theta,1,Par.n_reps) + reshape(360*(0:Par.n_reps-1).*ones(Par.n_theta,1),1,[]);
+                            FitMdl = fitnlm(double(th_fit'),repmat(Ia(:,:,frame)',Par.n_reps,1),...
+                                WFitEqn,StartVal(frame,:),'Weights',repmat(Ia(:,:,frame)',Par.n_reps,1), ...
+                                'Exclude',repmat(~idxa(:,:,frame)',Par.n_reps,1));
+                            Fits(:, frame) = FitMdl.Coefficients(:,1).Estimate;
+                            Errs(:, frame) = FitMdl.Coefficients(:,2).SE;
+                            ProgressBar(frame./N_Frs)
+                        end
+                    else
+                        for frame = 1:N_Frs
+                            % Take corresponding angles, repeat them and unwrap
+                            th_fit = repmat(Theta(idxa(:,:,frame)),1,Par.n_reps) + ...
+                                reshape(360*(0:Par.n_reps-1).*ones(length(Theta(idxa(:,:,frame))),1),1,[]);
+                            fitobj = fit(double(th_fit'),repmat(Ia(:,idxa(:,:,frame),frame)',Par.n_reps,1),FitEqn,...
+                                'Lower', lb, 'Upper', ub, 'Start', StartVal(frame,:),...
+                                'Exclude',repmat(~idxa(:,:,frame)',Par.n_reps,1));
+                            for VarN = 1:length(FitVars) % Extract fitted values from cfit object
+                                Fits(VarN, frame) = fitobj.(FitVars{VarN});
+                            end
+                            ConfInt = confint(fitobj);
+                            Errs(:,frame) = (ConfInt(2,:) - ConfInt(1,:))/2;
+                            prog = ceil(100 * frame / N_Frs);
+                            fprintf('%s\r',['[' repmat('=',1,prog) repmat(' ',1,100-prog) ']'])
+                        end
+                    end
+                else
+                    if Par.weighted
+                        warning('Weighted argument ignored because running parallel. Write some more code!')
+                    end
+                    n_reps = Par.n_reps;
+                    
+                    Ias = cell(N_Frs,1);
+                    Thetas = cell(N_Frs,1);
+                    for frame = 1:N_Frs
+                        Thetas{frame} = Theta(idxa(:,:,frame));
+                        Ias{frame} = Ia(:,idxa(:,:,frame))';
+                    end
+                    
+                    parfor frame = 1:N_Frs
+                        % Take corresponding angles, repeat them and unwrap
+                        th_fit = repmat(Thetas{frame},1,n_reps) + ...
+                            reshape(360*(0:n_reps-1).*ones(length(Thetas{frame}),1),1,[]);
+                        fitobj = fit(double(th_fit'),repmat(Ias{frame},n_reps,1),FitEqn,...
+                            'Lower', lb, 'Upper', ub, 'Start', StartVal(frame,:));
+                        
+                        Fits(:,frame) = coeffvalues(fitobj);
+                        ConfInt = confint(fitobj);
+                        Errs(:,frame) = (ConfInt(2,:) - ConfInt(1,:))/2;
+                    end
+                end
+        end
+        
+    end
+
+    function [Ia, idxa] = FindMaxes(Unwrapped, Radius, Par)
+        %% Find the cell edges in unwrapped data - either simple max method or broken gradient method
+        switch Par.edge_method
+            case 'simple'
+                [~, Ia] = max(Unwrapped(floor(Par.sc_up*max(Radius)*Par.sc_down):end,:,:));
+                Ia = Ia + floor(Par.sc_up*max(Radius)*Par.sc_down);
+                idxa = Ia > (1-Par.tol)*median(Ia,2) & Ia < (1+Par.tol)*median(Ia,2);
+            case 'gradient'
+                error('Unfinished edge_method')
+                UnwrapShrunk = Unwrapped(floor(Par.sc_up*max(Radius)*Par.sc_down):end,:,:);
+                Gy = zeros(size(UnwrappedShrunk));
+                Filt = zeros(size(UnwrappedShrunk)); % Why do I do this?
+                for frame = 1:size(UnwrappedShrunk,3)
+                    Filt(:,:,frame) = imgaussfilt(UnwrappedShrunk(:,:,frame),5,'FilterSize',[31,1]);
+                    [~, Gy(:,:,frame )] = imgradientxy(UnwrappedShrunk(:,:,frame)); % Should do the selection indexing here
+                end
+            case 'edge'
+                warning('edge_method ''edge'' not very effective')
+                Edges = zeros(size(Unwrapped));
+                for frame = 1:size(Unwrapped, 3)
+                    % Find edges from a radial region and place into temp array
+                    tmp = edge(Unwrapped(floor(Par.sc_up*max(Radius)*Par.sc_down):end,:,frame),'approxcanny');
+                    % Divide into connected regions
+                    [L, N] = bwlabel(tmp);
+                    % Count number of pixels in each region, select the largest and
+                    % place that region into edges array
+                    NPixels = squeeze(sum(L == reshape(1:N,1,1,N),[1,2]));
+                    [~, I] = max(NPixels);
+                    L(L~=I) = 0;
+                    % Index to place correctly in radial space
+                    Edges(floor(Par.sc_up*max(Radius)*Par.sc_down):end,:,frame) = L;
+                end
+                par = Par;
+                par.edge_method = 'simple';
+                [Ia, idxa] = FindMaxes(Edges, Radius, par);
+            case 'DoG_fitting'
+                Ia = zeros(size(Unwrapped(1,:,:)));
+                idxa = true(size(Ia));
+                
+                DoG = @(mu, sig, amp, x) amp * (x - mu) .* exp(-0.5.*((x-mu)/sig).^2)./(sqrt(2.*pi) .* sig.^3);
+                Start = [max(Radius), 0.1 * max(Radius), 1];
+                LB = [Par.sc_down * Par.sc_up * max(Radius), 0, -inf];
+                UB = [Par.sc_up * max(Radius), max(Radius), inf];
+                Rdata = 1:size(Unwrapped,1);
+                for frame = 1:size(Unwrapped,3)
+                    for theta = 1:size(Unwrapped,2)
+                        Idata = double(Unwrapped(:,theta,frame));
+                        Idata = Idata - (max(Idata) + min(Idata))/2;
+                        fitobj = fit(Rdata', Idata, DoG, ...
+                            'Lower', LB, 'Upper', UB, 'Start', Start);
+                        Ia(1,theta,frame) = fitobj.mu;
+                    end
+                end
+            otherwise
+                error('Unrecognised edge_method')
         end
     end
-    function [FitEqn, lb, ub, StartVal] = GetEqn(Func, Imstack, Par, Radius)
+
+    function [FitType, lb, ub, StartVal] = GetEqn(Func, Imstack, Par, Radius)
         %% Get an equation and bounds for fitting
         switch Func
             case 'circle'
-%                 switch Par.centering
-%                     case '2D'
+                switch Par.centering
+                    case '2D'
                         % Parametric eqn of off-centre circle (x = cos(t) + dx, y = sin(t) + dy),
                         % convert to polar (r = x.^2 + y.^2), insert DoG
                         % radial profile
@@ -216,11 +338,19 @@ end
                             amp .* (R - sqrt((r .* cos(0.0175.*Theta) + dx).^2 + (r .* sin(0.0175.*Theta) + dy).^2)) .* ...
                             exp(-((R - sqrt((r .* cos(0.0175.*Theta) + dx).^2 + (r .* sin(0.0175.*Theta) + dy).^2)).^2)./(2 .* sigma .^2)) ./ ...
                             (sqrt(2 .* pi) * sigma.^3);
+                        FitType = fittype(FitEqn,'independent',{'Theta','R'});
                         lb = [0, 0, 0, -size(Imstack{1}{1,1})/2];
                         ub = [Par.sc_up * max(Radius), inf, inf, size(Imstack{1}{1,1})/2];
                         StartVal = [Radius', repmat([5, 1e4, 0, 0],size(Radius,2), 1)];
-%                     case '1D'
-                        % 
+                    case '1D'
+                        % Parametric eqn of off-centre circle (x = cos(t) + dx, y = sin(t) + dy),
+                        % convert to polar (r = x.^2 + y.^2)
+                        FitEqn = @(r, dx, dy, x) sqrt((r .* cos(0.0175.*x) + dx).^2 + (r .* sin(0.0175.*x) + dy).^2);
+                        FitType = fittype(FitEqn);
+                        lb = [0, -size(Imstack{1}{1,1})/2];
+                        ub = [Par.sc_up * max(Radius), size(Imstack{1}{1,1})/2];
+                        StartVal = [Radius', repmat([0, 0],size(Radius,2), 1)];
+                end
             case 'ellipse'
                 % From parametric equation of ellipse (x = a cos(t), y = b sin(t)), take to
                 % polar, r = (a.cos(f * theta + phi))^2 +  (b.sin(f * theta + phi))^2,
@@ -237,6 +367,7 @@ end
                         amp .* (R - sqrt((a .* cos(0.0175.*Theta + phi)).^2 + (b .* sin(0.0175.*Theta + phi)).^2)) .* ...
                         exp(-((R - sqrt((a .* cos(0.0175.*Theta + phi)).^2 + (b .* sin(0.0175.*Theta + phi)).^2)).^2)./(2 .* sigma .^2)) ./ ...
                         (sqrt(2 .* pi) * sigma.^3);
+                    FitType = fittype(FitEqn,'independent',{'Theta','R'});
                     lb = [0, 0, 0, 0, 0];
                     ub = [inf, inf, pi, 100, inf];
                     StartVal = [repmat(Radius',1,2) repmat([1.5, 5, 1e4],size(Radius,2),1)]; % Use radius from find_cell as a start point
