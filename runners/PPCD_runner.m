6d2c09c2cc4dc569d21e52b774b097621f7bbed9
will hardiman <bill.hardiman1995@gmail.com>
2020-07-10 11:45:41 +0100

    settings

--- runners/PPCD_runner.m
+++ runners/PPCD_runner.m
@@ -3,8 +3,8 @@ function PPCD_runner(varargin)
 
 Par = [];
 % Which datasets
-CellDefault = {'HL60'}; % {'LS174T','HL60','MV411'};
-DSetsDefault = {{'normoxia','with_drugs'}}; % {{'normoxia','hypoxia'},{'normoxia','with_drugs'},{'normoxia','with_drugs'}};
+CellDefault = {'MV411'}; % {'LS174T','HL60','MV411'};
+DSetsDefault = {{'with_drugs'}}; % {{'normoxia','hypoxia'},{'normoxia','with_drugs'},{'normoxia','with_drugs'}};
 NumsDefault = {{1:30, 1:20}}; % {{1:20, 1:20},{1:30, 1:20},{1:30, 1:20}};
 
 % What settings
