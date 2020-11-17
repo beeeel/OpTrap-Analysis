09b14903eae980fe2b6fe8e3dd80edb644af22a6
will hardiman <bill.hardiman1995@gmail.com>
2020-11-17 11:28:30 +0000

    added stretch augmentation

--- junk/CreateMLSampleSet.m
+++ junk/CreateMLSampleSet.m
@@ -1,15 +1,19 @@
-%% Take some images out to create training dataset
+ %% Take some images out to create training dataset
 % Output images directory
 SaveDir = '~/AI_Optrap/Samples/Set1/';
 % Dummy class - images in this class are all 0s. 
 % 'stretched' or 'relaxed'. Anything else will be ignored/treated as none
 DummyClass = 'none';
-% Output size expression to be evaluated within the deepest loop
-% OutSizeExp = '';
-% Augmentation
-% Random shift - up to 30 px in any direction, applied to every image
-% (doubles size of dataset)
-RandomShift = 30;
+% Output size expression to be evaluated within the deepest loop 
+% OutSizeExp = ''; 
+
+%Augmentation:
+% Random shift - N px in random direction, applied to every image (doubles
+% size of dataset)
+RandomShift = 0;
+% Stretch - Use interpolation to stretch fractionally. [1 2] would double
+% the width, for example. Only applied to "stretched" image class.
+StretchRatio = [1 1];
 
 
 % Which datasets to take images from
@@ -17,14 +21,15 @@ Cells = {'LS174T', 'HL60', 'MV411'};
 DSets = {{'normoxia','hypoxia'},{'normoxia','with_drugs'},{'normoxia','with_drugs'}};
 Nums = {{1:20, 1:20},{1:30, 1:20},{1:30, 1:20}};
 % How many images to take from each dataset
-FramesPerSet = 10;
+FramesPerSet = 150;
 
 % Calculate how many images there will be
 AllNums = [Nums{:}];
 NumSets = length([AllNums{:}]);
-NumIms = FramesPerSet * NumSets;
+NumIms = FramesPerSet * NumSets; % Number of unique images
+TotalIms = NumIms * (1 + (RandomShift ~= 0) + 0.5 * max(StretchRatio ~= 1)); % Number of images after augmentation
 
-SelectedIms = cell(NumIms * (1 + (RandomShift ~= 0)), 2);
+SelectedIms = cell(TotalIms, 2);
 Count = 0;
 global Imstack info meta
 StartTime = tic;
@@ -62,6 +67,7 @@ for CTidx = 1:length(Cells)
                 ImInfo.FrNum = frame + Offset; %frame * BlockSize + Offset;
                 ImInfo.ImW = size(Imstack{1}{ImInfo.FrNum},2);
                 ImInfo.ImH = size(Imstack{1}{ImInfo.FrNum},1);
+                ImInfo.Augment = '';
                 
                 % Round up output size to nearest power of 2
                 %OutSize = repmat(2^ceil(log2(max(size(Imstack{1}{1,1})))),1,2);
@@ -79,15 +85,28 @@ for CTidx = 1:length(Cells)
                 SelectedIms{Count,2} = ImInfo;
                 % If RandomShift is a variable with a value
                 if exist('RandomShift','var') == 1 && RandomShift
-                    SelectedIms{Count+NumIms,1} = CropAndShift(...
+                    Count = Count + 1;
+                    SelectedIms{Count,1} = CropAndShift(...
                         Imstack{1}{ImInfo.FrNum}, OutSize, RandomShift);
-                    SelectedIms{Count+NumIms,2} = ImInfo;
+                    SelectedIms{Count,2} = ImInfo;
+                    SelectedIms{Count,2}.Augment = 'Translation';
+                end
+                % If StretchRatio is a variable with at least one non-one
+                % value AND we're selecting a stretched frame
+                if exist('StretchRatio','var') == 1 && Offset ~= 0 && max(StretchRatio ~= 1)
+                    Count = Count + 1;
+                    SelectedIms{Count,1} = CropAndStretch(...
+                        Imstack{1}{ImInfo.FrNum},OutSize, StretchRatio);
+                    SelectedIms{Count,2} = ImInfo;
+                    SelectedIms{Count,2}.Augment = 'Stretch';
                 end
             end
         end
     end
 end
 toc(StartTime)
+Vars = whos;
+fprintf('Mem size of SelectedIms cell: %g GB\n',Vars(strcmp({Vars.name},'SelectedIms')).bytes./1e9)
 %% Augment - WIP
 % Take a circular mask, interpolate the sample to create deformation, stick
 % back on top of the image
@@ -109,9 +128,9 @@ elseif ~strcmp(SaveDir(end),'/')
     SaveDir = [SaveDir '/'];
 end
 
-for Imidx = 1:Count
+for Imidx = 1:TotalIms
     II = SelectedIms{Imidx,2};
-    if II.FrNum > 500
+    if II.FrNum > 500 || strcmp(II.Augment,'Stretch')
         Dir = [SaveDir 'stretched/'];
         ThisImClass = 'stretched';
         % These lines (and 2 in the else) are for file names as 1,2,3...
@@ -123,9 +142,16 @@ for Imidx = 1:Count
 %         FName = num2str(RelaxedCount);
 %         RelaxedCount = RelaxedCount + 1;
     end
-    FName = strjoin({II.CellType, II.Set, II.Num, num2str(II.FrNum)},'_');
+    if isempty(II.Augment)
+        Aug = '';
+    else
+        Aug = II.Augment(1);
+    end
+    FName = strjoin({II.CellType, II.Set, II.Num, num2str(II.FrNum), Aug},'_');
 %     if strcmp(DummyClass,ThisImClass)
+    if true%(strcmp(ThisImClass,'relaxed') && isempty(II.Augment)) || strcmp(II.Augment,'Stretch')
         imwrite(uint8(SelectedIms{Imidx,1}),[Dir FName '.png'])
+    end
 %     else
 %         imwrite(zeros(II.ImH,II.ImW,'uint8'),[Dir FName '.png'])
 %     end
@@ -139,10 +165,10 @@ for Imidx = 1:Count
 end
 
 function ImOut = CropAndShift(ImIn, OutSize, ShiftMag)
-    %% Crop and potentially shift (in x,y) an input image, returning a uint8 array
+    %% Crop and potentially shift (in x,y) an input image, returning array of matching type
     % Get image size and calculate range of indices to take
     [ImH, ImW] = size(ImIn);
-    if sum(OutSize > [ImH, ImW]) > 0
+    if max(OutSize > [ImH, ImW])
         fprintf('Output size: [%i, %i].\nInput size: [%i, %i].\n',...
             OutSize(1), OutSize(2), ImH, ImW)
         error('Requested output size larger than input size')
@@ -168,4 +194,31 @@ function ImOut = CropAndShift(ImIn, OutSize, ShiftMag)
         disp('a');
     end
     ImOut = ImIn(Yidx(1):Yidx(2),Xidx(1):Xidx(2));
+end
+
+function ImOut = CropAndStretch(ImIn, OutSize, StretchRatio)
+    %% Crop and stretch an input image, returning array of the same type
+    [ImH, ImW] = size(ImIn);
+    if max(OutSize > [ImH, ImW])
+        fprintf('Output size: [%i, %i].\nInput size: [%i, %i].\n',...
+            OutSize(1), OutSize(2), ImH, ImW)
+        error('Requested output size larger than input size')
+    end
+    % Input image space
+    X = 1:ImW;
+    Y = 1:ImH;
+    % Sampling points - start at the centre (ImW/2 e.g.) subtract half the
+    % stretched space (which is smaller than image space, hence divide) to
+    % find the input space co-ordinate for the edge of the output image.
+    Xq = linspace(floor(ImW/2 - OutSize(2)/(2*StretchRatio(2))), ...
+        floor(ImW/2 + OutSize(2)/(2*StretchRatio(2)))+1, ...
+        OutSize(2));
+    Yq = linspace(floor(ImH/2 - OutSize(1)/(2*StretchRatio(1))), ...
+        floor(ImH/2 + OutSize(1)/(2*StretchRatio(1)))+1, ...
+        OutSize(1));
+    % Create sampling meshes and do interpolation. Interpolation requires
+    % input of class double and I want ImOut to be the same class as ImIn.
+    [XXq, YYq] = meshgrid(Xq, Yq);
+    ImOut = interp2(X, Y, double(ImIn), XXq, YYq);
+    ImOut = cast(ImOut, 'like', ImIn);
 end
