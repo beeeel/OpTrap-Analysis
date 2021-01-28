043421536066e2f9573c3aac171b560b3976351b
will hardiman <bill.hardiman1995@gmail.com>
2021-01-28 12:30:44 +0000

    load full images when available

--- beads/bead_loadData.m
+++ beads/bead_loadData.m
@@ -6,6 +6,11 @@ data.raw.xCentresPx = byteStreamToDouble([data.dirPath '/XCentres.dat']);
 data.raw.yCentresPx = byteStreamToDouble([data.dirPath '/YCentres.dat']);
 data.raw.timeVecMs  = byteStreamToDouble([data.dirPath '/Times.dat']);
 data.Imstack  = bfopen([data.dirPath '/images_and_metadata/images_and_metadata_MMStack_Default.ome.tif']);
+if exist([data.dirPath '/full_images_and_metadata/full_images_and_metadata_MMStack_Default.ome.tif'], 'file')
+    data.ImstackFullFoV  = bfopen([data.dirPath '/full_images_and_metadata/full_images_and_metadata_MMStack_Default.ome.tif']);
+else
+    warning('Could not find full FoV images')
+end
 metadata = fileread([data.dirPath '/images_and_metadata/images_and_metadata_MMStack_Default_metadata.txt']);
 data.metadata = jsondecode(metadata);
 data.nPoints = length(data.raw.xCentresPx);
