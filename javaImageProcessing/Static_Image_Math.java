22de5ea6cd5a16dc4c3bc7f7dded288f6cad48d0
beeeel <50520111+beeeel@users.noreply.github.com>
2021-02-04 16:22:32 +0000

    Create static Java getMean for noise investigation

--- javaImageProcessing/Static_Image_Math.java
+++ javaImageProcessing/Static_Image_Math.java
@@ -74,4 +74,24 @@ public static double[] getDarkCentroid(short[] pixels, int width, int height) {
         CenterOfMass[1] = y2sum/sum2+0.5;
         return CenterOfMass;
     }
+
+
+public static double getMean(short[] pixels, int width, int height) {
+        // Finds mean of image
+        double mean;
+        int i, count = 0;
+        double v, sum = 0.0;
+        
+        for (int y = 0; y < height; y++) {
+            i = y * width;
+            for (int x = 0; x < width; x++) {
+                 v = pixels[i];
+                 sum += v;
+                 i ++;
+                 count ++;
+            }
+        }
+        mean = sum / count;
+        return mean;
+        }
 }
