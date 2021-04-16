467c768fb9d22f037c1ce96611efa5b04bc598e7
will hardiman <bill.hardiman1995@gmail.com>
2021-04-16 15:49:26 +0100

    fixed plotting of multiple sets of centres data

--- beads/bead_plotRawData.m
+++ beads/bead_plotRawData.m
@@ -3,7 +3,9 @@ function fh = bead_plotRawData(data, varargin)
 % Do histograms and scatterplots for 2D position/time data
 
 xCentresM = data.raw.xCentresPx * data.mPerPx;
+xCentresM = xCentresM - mean(xCentresM, 2);
 yCentresM = data.raw.yCentresPx * data.mPerPx;
+yCentresM = yCentresM - mean(yCentresM, 2);
 timeVec = data.raw.timeVecMs;
 cropT = data.opts.cropT;      
 
@@ -28,46 +30,100 @@ fh.Name = data.fName;
 clf
 
 % Histogram of xCentres and yCentres in units um
-subplot(3,1,1)
-hold on
-histogram(xCentresM.*1e6,'Normalization','probability')
-histogram(yCentresM.*1e6,'Normalization','probability')
-if setLims
-    xlim([-1 1] * 0.05)
-end
-xlabel('Centre position (\mu m)')
-ylabel('Probability')
-order = num2str(data.opts.pOrder);
-title({['Histogram of centres, unfiltered'], [ 'trap stiffness kx = ' num2str(xStiff./1e-6) ' pN/\mu m, ky = ' num2str(yStiff./1e-6) ' pN/\mu m']})
-legend('X','Y')
-
-% Scatterplot of each centre in units um
-subplot(3,1,2)
-plot(xCentresM.*1e6,yCentresM.*1e6,'.')
-if setLims
-    xlim([-1 1] * 0.15)
-    ylim([-1 1] * 0.15)
-end
-title(['Scatterplot of centres, ' num2str(length(xCentresM)) ' frames'])
-xlabel('X (\mu m)')
-ylabel('Y (\mu m)')
-axis equal
-
-% Time traces of X and Y in units um
-subplot(3,2,5)
-plot(1e-3.*timeVec(cropT(1):cropT(2)), xCentresM.*1e6,'.')
-xlabel('Time (s)')
-ylabel('X (\mu m)')
-title('X time trace')
-if setLims
-    ylim([-1 1]*0.15)
-end
-subplot(3,2,6)
-plot(1e-3.*timeVec(cropT(1):cropT(2)), yCentresM.*1e6,'.')
-xlabel('Time (s)')
-ylabel('Y (\mu m)')
-title('Y time trace')
-if setLims
-    ylim([-1 1]*0.15)
+if size(xCentresM, 1) == 1
+    subplot(3,1,1)
+    hold on
+    histogram(xCentresM.*1e6,'Normalization','probability')
+    histogram(yCentresM.*1e6,'Normalization','probability')
+    if setLims
+            xlim(setLims)
+    end
+    xlabel('Centre position (\mu m)')
+    ylabel('Probability')
+    order = num2str(data.opts.pOrder);
+    title({['Histogram of centres, unfiltered'], [ 'trap stiffness kx = ' num2str(xStiff(1)./1e-6) ' pN/\mu m, ky = ' num2str(yStiff(1)./1e-6) ' pN/\mu m']})
+    legend('X','Y')
+    
+    % Scatterplot of each centre in units um
+    subplot(3,1,2)
+    plot(xCentresM.*1e6,yCentresM.*1e6,'.')
+    if setLims
+        xlim(setLims)
+        ylim(setLims)
+    end
+    title(['Scatterplot of centres, ' num2str(length(xCentresM)) ' frames'])
+    xlabel('X (\mu m)')
+    ylabel('Y (\mu m)')
+    axis equal
+    
+    % Time traces of X and Y in units um
+    subplot(3,2,5)
+    plot(1e-3.*timeVec(cropT(1):cropT(2)), xCentresM.*1e6,'.')
+    xlabel('Time (s)')
+    ylabel('X (\mu m)')
+    title('X time trace')
+    if setLims
+        ylim(setLims)
+    end
+    subplot(3,2,6)
+    plot(1e-3.*timeVec(cropT(1):cropT(2)), yCentresM.*1e6,'.')
+    xlabel('Time (s)')
+    ylabel('Y (\mu m)')
+    title('Y time trace')
+    if setLims
+        ylim(setLims)
+    end
+else
+    for obj = 1:size(xCentresM,1)
+        % Histogram of xCentres and yCentres in units um
+        subplot(3,1,1)
+        hold on
+        histogram(xCentresM(obj,:).*1e6,'Normalization','probability')
+        histogram(yCentresM(obj,:).*1e6,'Normalization','probability')
+        if ~isempty(setLims)
+            xlim(setLims)
+        end
+        xlabel('Centre position (\mu m)')
+        ylabel('Probability')
+        title({'Histogram of centres, unfiltered' [ 'trap stiffness kx = ' num2str(xStiff(1)./1e-6) ' pN/\mu m, ky = ' num2str(yStiff(1)./1e-6) ' pN/\mu m']})
+        warning('off','MATLAB:legend:IgnoringExtraEntries')
+        legend('X left','Y left','X right','Y right')
+        warning('on','MATLAB:legend:IgnoringExtraEntries')
+        
+        % Scatterplot of each centre in units um
+        subplot(3,1,2)
+        hold on
+        scatter(xCentresM(obj,:).*1e6,yCentresM(obj,:).*1e6,[],1e-3.*timeVec(cropT(1):cropT(2)),'.')
+        if ~isempty(setLims)
+            xlim(setLims)
+            ylim(setLims)
+        end
+        title(['Scatterplot of centres, ' num2str(length(xCentresM)) ' frames'])
+        xlabel('X (\mu m)')
+        ylabel('Y (\mu m)')
+        axis equal
+        colorbar
+        
+        % Time traces of X and Y in units um
+        subplot(3,2,5)
+        hold on
+        plot(1e-3.*timeVec(cropT(1):cropT(2)), xCentresM(obj,:).*1e6,'.')
+        xlabel('Time (s)')
+        ylabel('X (\mu m)')
+        title('X time trace')
+        if ~isempty(setLims)
+            ylim(setLims)
+        end
+        
+        subplot(3,2,6)
+        hold on
+        plot(1e-3.*timeVec(cropT(1):cropT(2)), yCentresM(obj,:).*1e6,'.')
+        xlabel('Time (s)')
+        ylabel('Y (\mu m)')
+        title('Y time trace')
+        if ~isempty(setLims)
+            ylim(setLims)
+        end
+    end
 end
 drawnow
