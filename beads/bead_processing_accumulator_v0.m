21851210b12f6a46107e71c0da22c6edeeb56c05
will hardiman <bill.hardiman1995@gmail.com>
2021-06-07 12:53:21 +0100

    Finished(ish) for now

--- beads/bead_processing_accumulator_v0.m
+++ beads/bead_processing_accumulator_v0.m
@@ -3,15 +3,28 @@ close all
 
 % Data storage parameters
 masterDir = '~/Data/phd/OpTrap/';           % Where all the data is
-dayDirs = {'2021_05_17/' '2021_05_13/' '2021_04_22/'};    % Which day's data to load
-setIdxs = {{1:3 5:7 10:12} {3:6 7:9 10:13 16:19 20:22 23:25} {1:5 6:8 }};
+saveDir = 'accumulator_01/';                % Where to save processed data
+dayDirs = {'2021_05_17/' '2021_05_13/'};    % Which day's data to load
+
+% This cell contains 1 cell per dayDir above. Within that needs to be
+% indexes for that day's dirList, to choose the correct datasets.
+setIdxs = {{1:3 5:7 10:12} ...
+    {3:6 7:9 10:13 16:19 20:22 23:25}};
+
+% Ain't nobody got time (be)for this!
+% Later I will record time in a filename when recording data
+thymes = { 
+    ... % Times from 2021_05_17
+        {[42 95 161] [80 103 142] [91 107 156]} ...
+    ... % Times from 2021_05_13
+        {[81 155 167 224] [87 137 148] [95 98 160 172] [119 123 178 207] [141 182 211] [147 186 215]}};
+
 % Experiment parameters
 mPerPx = 0.07e-6;           % Camera pixel size calibration
-laserPowers = 0;      % Laser power in % for the datasets used
 ignoreDirs = {'focal_sweep_with_bead'}; % Directories to ignore (ones without data)
 
 % Processing parameters
-cropTs = {};
+cropTs = {[]};
 fitPoly = 1; % Fit a polynomial to remove drift.
 
 fitPolyOrder = 18;      % Order of polynomial to be fitted
@@ -32,97 +45,112 @@ forceRun = true;       % Try to take data from file and reuse as much as possibl
 saveData = false;        % Save data to file
 dataSuff = '_simple';       % Suffix for filename when saving/loading
 
-% Prepare for trouble
-out = struct();
-% And make it double!
-out(1).stiff = double(nan);
+% ARE YOU READY??
+out = cell(size(dayDirs));
+if ~exist([masterDir saveDir], 'dir')
+    mkdir([masterDir saveDir])
+end
+cd([masterDir saveDir])
+fprintf('Working in %s\n',pwd)
+fileCount = 1;
 
 % For each day chosen
 for dayIdx = 1:length(dayDirs)
-    % Go to the correct directory
+    % Get the correct directory
     dayDir = [masterDir dayDirs{dayIdx}];
-    cd(dayDir);
     
     % Get all the children directories in a struct
-    dirList = dir;
+    dirList = dir(dayDir);
     dirList = dirList([dirList.isdir]);
     dirList = dirList(3:end);
     for d = 1:length(ignoreDirs)
         dirList = dirList(~strcmp({dirList.name},ignoreDirs{d}));
     end
     
-    for fileIdx = setIdxs{dayIdx}
+    % Prepare for trouble
+    out{dayIdx} = {};
+    
+    for cellIdx = 1:length(setIdxs{dayIdx})
+        % Prepare for trouble
+        out{dayIdx}{1,cellIdx} = struct();
+        % And make it double
+        out{dayIdx}{cellIdx}.stiff = double(nan);
         
-        %% Load and pre-process
-        % Either create a new struct or load one named dataFile
-        dataFile = [dirList(fileIdx).name '_processed' dataSuff '.mat'];
-        if forceRun || ~exist(dataFile, 'file')
-            data = struct([]);
-            data(1).opts.forceRun = forceRun;
+        out{dayIdx}{2,cellIdx} = thymes{dayIdx}{cellIdx};
+        
+        for fIdx = 1:length(setIdxs{dayIdx}{cellIdx})
+            fileIdx = setIdxs{dayIdx}{cellIdx}(fIdx);
             
-            % Set names and load data
-            data.dirPath = [dirList(fileIdx).folder '/' dirList(fileIdx).name];
-            data.fName = dirList(fileIdx).name;
-            data = bead_loadData(data);
+            %% Load and pre-process
+            % Either create a new struct or load one named dataFile
+            dataFile = [dirList(fileIdx).folder '/' dirList(fileIdx).name '_processed' dataSuff '.mat'];
+            if forceRun || ~exist(dataFile, 'file')
+                data = struct([]);
+                data(1).opts.forceRun = forceRun;
+                
+                % Set names and load data
+                data.dirPath = [dirList(fileIdx).folder '/' dirList(fileIdx).name];
+                data.fName = dirList(fileIdx).name;
+                data = bead_loadData(data, false);
+                
+                % Apply calibration and crop time
+                data.opts.cropT = cropTs{1};
+                data.opts.pOrder = fitPolyOrder*fitPoly;
+                data.mPerPx = mPerPx;
+            else
+                tmp = load(dataFile, 'data');
+                data = tmp.data;
+                clear tmp
+                data.opts.forceRun = forceRun;
+            end
             
-            % Apply calibration and crop time
-            data.opts.cropT = cropTs{fileIdx};
-            data.opts.pOrder = fitPolyOrder*fitPoly(fileIdx);
-            data.mPerPx = mPerPx;
-        else
-            tmp = load(dataFile, 'data');
-            data = tmp.data;
-            clear tmp
-            data.opts.forceRun = forceRun;
+            % Apply scale and do polyfit
+            data = bead_preProcessCentres(data);
+            %% Process data
+            % Calculate the stiffnesses and put into data
+            if calcStiff
+                stiffIdx = 1;
+                out{dayIdx}{cellIdx}(fIdx).suffix = data.raw.suffixes{stiffIdx};
+                
+                xStiff = calcStiffness(data.pro.xCentresM);
+                yStiff = calcStiffness(data.pro.yCentresM);
+                data.pro.stiffXYpro = [xStiff, yStiff];
+                out{dayIdx}{cellIdx}(fIdx).stiff = data.pro.stiffXYpro(stiffIdx,:);
+                
+                xStiff = calcStiffness(data.raw.xCentresPx, data.mPerPx);
+                yStiff = calcStiffness(data.raw.yCentresPx, data.mPerPx);
+                data.pro.stiffXYraw = [xStiff, yStiff];
+                out{dayIdx}{cellIdx}(fIdx).stiffraw = data.pro.stiffXYraw(stiffIdx,:);
+            end
+            
+            %% High-pass filter and calculate Allan variance if pass frequency is
+            % positive
+            if fpass > 0
+                data.opts.fpass = fpass;
+                data.opts.cropTHPval = cropTHPval;
+                % Need to adapt to run on both dims and use subplots/etc
+                data = bead_hp_allan_var(data, 'xCentresPx', ...
+                    fpass, cropTHPval, false);
+                data = bead_hp_allan_var(data, 'yCentresPx', ...
+                    fpass, cropTHPval, false);
+                
+                xStiff = calcStiffness(data.pro.xCentresHP);
+                yStiff = calcStiffness(data.pro.yCentresHP);
+                data.pro.stiffXYHP = [xStiff, yStiff];
+                out{dayIdx}{cellIdx}(fIdx).stiffHP = data.pro.stiffXYHP;
+                
+            end
+            
+            % Look at mean-square displacement (for cell-bead expts)
+            if ~isempty(msdOffset)
+                data = bead_normMSD_polyfit(data, msdDim, msdOffset, msdNumT, false, msdUseRaw, msdCentresRow, msdDoNorm);
+            end
+            
+            % Save if requested
+            if saveData
+                save(dataFile, 'data')
+            end
+            
         end
-        
-        % Apply scale and do polyfit
-        data = bead_preProcessCentres(data);
-        %% Process data
-        % Calculate the stiffnesses and put into data
-        if calcStiff
-            stiffIdx = 1;
-            out(fileIdx).suffix = data.raw.suffixes{stiffIdx};
-            
-            xStiff = calcStiffness(data.pro.xCentresM);
-            yStiff = calcStiffness(data.pro.yCentresM);
-            data.pro.stiffXYpro = [xStiff, yStiff];
-            out(fileIdx).stiff = data.pro.stiffXYpro(stiffIdx,:);
-            
-            xStiff = calcStiffness(data.raw.xCentresPx, data.mPerPx);
-            yStiff = calcStiffness(data.raw.yCentresPx, data.mPerPx);
-            data.pro.stiffXYraw = [xStiff, yStiff];
-            out(fileIdx).stiffraw = data.pro.stiffXYraw(stiffIdx,:);
-        end
-        
-        %% High-pass filter and calculate Allan variance if pass frequency is
-        % positive
-        if fpass > 0
-            data.opts.fpass = fpass;
-            data.opts.cropTHPval = cropTHPval;
-            % Need to adapt to run on both dims and use subplots/etc
-            data = bead_hp_allan_var(data, 'xCentresPx', ...
-                fpass, cropTHPval, false);
-            data = bead_hp_allan_var(data, 'yCentresPx', ...
-                fpass, cropTHPval, false);
-            
-            xStiff = calcStiffness(data.pro.xCentresHP);
-            yStiff = calcStiffness(data.pro.yCentresHP);
-            data.pro.stiffXYHP = [xStiff, yStiff];
-            out(fileIdx).stiffHP = data.pro.stiffXYHP(2,:);
-            
-        end
-        
-        % Look at mean-square displacement (for cell-bead expts)
-        if ~isempty(msdOffset)
-            data = bead_normMSD_polyfit(data, msdDim, msdOffset, msdNumT, false, msdUseRaw, msdCentresRow, msdDoNorm);
-        end
-        
-        % Save if requested
-        if saveData
-            save(dataFile, 'data')
-        end
-        
     end
 end
-
