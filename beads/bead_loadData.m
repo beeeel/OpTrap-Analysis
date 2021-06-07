99e5a314408b7969a1e4554640a31e2d0fd019ee
will hardiman <bill.hardiman1995@gmail.com>
2021-06-07 12:52:32 +0100

    Added option to not load TIF images (speeds things up)

--- beads/bead_loadData.m
+++ beads/bead_loadData.m
@@ -1,25 +1,11 @@
-function data = bead_loadData(data)
-%% data = loadBeadData(data)
+function data = bead_loadData(data, varargin)
+%% data = loadBeadData(data, [loadImages])
 % Load centres, times, images and metadata for a given dataset
 
-% % Old method - manually look for data
-% if exist([data.dirPath '/XCentres.dat'],'file')
-%     data.raw.xCentresPx = byteStreamToDouble([data.dirPath '/XCentres.dat']);
-%     data.raw.yCentresPx = byteStreamToDouble([data.dirPath '/YCentres.dat']);
-% elseif exist([data.dirPath '/X.dat'], 'file')
-%     data.raw.xCentresPx = byteStreamToDouble([data.dirPath '/X.dat']);
-%     data.raw.yCentresPx = byteStreamToDouble([data.dirPath '/Y.dat']);
-% elseif exist([data.dirPath '/Xl.dat'], 'file')
-%     data.raw.xCentresPx = byteStreamToDouble([data.dirPath '/Xl.dat']);
-%     data.raw.yCentresPx = byteStreamToDouble([data.dirPath '/Yl.dat']);
-%     data.raw.xCentresPx(2,:) = byteStreamToDouble([data.dirPath '/Xr.dat']);
-%     data.raw.yCentresPx(2,:) = byteStreamToDouble([data.dirPath '/Yr.dat']);
-% else
-%     disp(['File ' data.dirPath '/XCentres.dat does not exist'])
-%     disp(['File ' data.dirPath '/X.dat does not exist'])
-%     disp(['File ' data.dirPath '/Xl.dat does not exist'])
-%     error('Could not find any centres data');
-% end
+loadImages = true;
+if nargin > 1
+    loadImages = varargin{1};
+end
 
 % New method: Find anything with a suffix and then record it and what
 % suffix
@@ -72,22 +58,26 @@ else
     error('Could not find any times data');
 end
 
-if exist([data.dirPath '/images_and_metadata/images_and_metadata_MMStack_Default.ome.tif'], 'file')
-    data.Imstack  = bfopen([data.dirPath '/images_and_metadata/images_and_metadata_MMStack_Default.ome.tif']);
-    metadata = fileread([data.dirPath '/images_and_metadata/images_and_metadata_MMStack_Default_metadata.txt']);
-    data.metadata = jsondecode(metadata);
-elseif exist([data.dirPath '/ROI/ROI_MMStack_Default.ome.tif'], 'file')
-    data.Imstack  = bfopen([data.dirPath '/ROI/ROI_MMStack_Default.ome.tif']);
+if loadImages
+    if exist([data.dirPath '/images_and_metadata/images_and_metadata_MMStack_Default.ome.tif'], 'file')
+        data.Imstack  = bfopen([data.dirPath '/images_and_metadata/images_and_metadata_MMStack_Default.ome.tif']);
+        metadata = fileread([data.dirPath '/images_and_metadata/images_and_metadata_MMStack_Default_metadata.txt']);
+        data.metadata = jsondecode(metadata);
+    elseif exist([data.dirPath '/ROI/ROI_MMStack_Default.ome.tif'], 'file')
+        data.Imstack  = bfopen([data.dirPath '/ROI/ROI_MMStack_Default.ome.tif']);
+    else
+        warning('Could not find ROI images or metadata')
+    end
+    
+    if exist([data.dirPath '/full_images_and_metadata/full_images_and_metadata_MMStack_Default.ome.tif'], 'file')
+        data.ImstackFullFoV  = bfopen([data.dirPath '/full_images_and_metadata/full_images_and_metadata_MMStack_Default.ome.tif']);
+    elseif exist([data.dirPath '/full/full_MMStack_Default.ome.tif'], 'file')
+        data.ImstackFullFoV  = bfopen([data.dirPath '/full/full_MMStack_Default.ome.tif']);
+    else
+        warning('Could not find full FoV images')
+    end
 else
-    warning('Could not find ROI images or metadata')
-end
-
-if exist([data.dirPath '/full_images_and_metadata/full_images_and_metadata_MMStack_Default.ome.tif'], 'file')
-    data.ImstackFullFoV  = bfopen([data.dirPath '/full_images_and_metadata/full_images_and_metadata_MMStack_Default.ome.tif']);
-elseif exist([data.dirPath '/full/full_MMStack_Default.ome.tif'], 'file')
-    data.ImstackFullFoV  = bfopen([data.dirPath '/full/full_MMStack_Default.ome.tif']);
-else
-    warning('Could not find full FoV images')
+    warning('Instructed to not load images')
 end
 
 data.nPoints = length(data.raw.xCentresPx);
