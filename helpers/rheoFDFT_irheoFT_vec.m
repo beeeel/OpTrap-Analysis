982ac37a4413576898fb04d0664d1a02f3ecf778
will hardiman <bill.hardiman1995@gmail.com>
2021-08-19 09:26:05 +0100

    fixed some mistakes from the original code

--- helpers/rheoFDFT_irheoFT_vec.m
+++ helpers/rheoFDFT_irheoFT_vec.m
@@ -6,7 +6,7 @@ gdata_I = zeros(freqpoints,3);
 
 GFT = zeros(freqpoints,3);
 
-wrange = logspace(-2,2,freqpoints); % Freqency Range
+wrange = logspace(log10(t_I(1)),log10(t_I(end)),freqpoints); % Freqency Range
 
 for n = 1:freqpoints
     w = wrange(n);
@@ -17,13 +17,25 @@ for n = 1:freqpoints
 %             (exp(-1i*w*t_I(k-1))-exp(-1i*w* t_I(k))));
 %     end
 
-    % New
+    % New: vectorized for t_I
+    
     A = ( diff(Gint_I) ./ diff(t_I) ) ...
         .* ( exp( -1i .* w .* t_I(1:end-1) ) - exp( -1i .* w .* t_I(2:end) ) );
     
-    GFT1 = ((1i*w*g0+((1-exp(-1i*w* t_I(2)))*((Gint_I(1)-g0)/t_I(2)))+...
-        ginf*exp(-1i*w*t_I(end)))+sum(A))/(1i*w)^2;
-    
+    % Original - uses t_I(2)
+%     GFT1 = ((1i*w*g0 + ...
+%         ((1-exp(-1i*w* t_I(2)))*((Gint_I(1)-g0)/t_I(2))) + ...
+%         ginf*exp(-1i*w*t_I(end))) + ...
+%         sum(A)) ...
+%         / (1i*w)^2;
+
+    % I think it should use t_I(1), but it doesn't make a visible
+    % difference
+    GFT1 = ((1i*w*g0 + ...
+        ((1-exp(-1i*w* t_I(1)))*((Gint_I(1)-g0)/t_I(1))) + ...
+        ginf*exp(-1i*w*t_I(end))) + ...
+        sum(A)) ...
+        / (1i*w)^2;
     Gstar = GFT1*(1i*w);
     GFT(n,:) = [w real(GFT1) imag(GFT1)];
     gdata_I(n,:) = [w real(Gstar) imag(Gstar)];
