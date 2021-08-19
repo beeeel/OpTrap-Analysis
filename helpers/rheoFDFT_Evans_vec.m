30e165c298721d85c9e415eacf3cfbcf5c97288d
will hardiman <bill.hardiman1995@gmail.com>
2021-08-19 09:19:43 +0100

    Improved memory checking and omega spacing

--- helpers/rheoFDFT_Evans_vec.m
+++ helpers/rheoFDFT_Evans_vec.m
@@ -1,4 +1,4 @@
-function [Y] = rheoFDFT_Evans_vec(tau, msd, J0, eta)
+function [omega, Y] = rheoFDFT_Evans_vec(tau, msd, nOmegas, J0, eta)
 %Y = rheoFDFT_Evans_vec(tau, msd, J0, eta)
 %% Compute finite discrete fourier transform using Evans et al 2009
 % Requires consideration of the long-time limit of the gradient = 1/η
@@ -19,48 +19,65 @@ if tau(1) == 0 || min(msd(1,:) == 0)
     error('Expected non-zero lag time and MSDs')
 end
 
-% Setup
-omega = 2*pi./tau;
+% Setup: Frequency sampling
+omega = 1./logspace(log10(tau(1)), log10(tau(end)), nOmegas)';
+% omega = 2*pi./tau;
 
 %% Find out how much this can be parallelized
 % Determine max array size by asking for an array of increasing size
-n = 0;
-m = 1;
-state = true;
-while state && (m*10^n) < N^2
-    n = n + 1;
-    try
-        [~] = zeros(N, m*10^n);
-    catch
-        n = n - 1;
-        while state
-            m = m + 0.1;
-            try
-                [~] = zeros(N, m*10^n);
-            catch
-                m = m - 0.1;
-                state = false;
+try
+    % First see if we can do it fully vectorized (most memory intense)
+    [~] = zeros(2*N, nOmegas);
+    % Indexes for summation term
+    idx = 1;
+    edx = N;
+    nEl = N;
+catch
+    % If that fails, incrementally ask for bigger arrays until it fails
+    n = 0;
+    m = 1;
+    state = true;
+    while state && (m*10^n) < nOmegas^2
+        % Big steps
+        n = n + 1;
+        try
+            [~] = zeros(nOmegas, m*10^n);
+        catch
+            n = n - 1;
+            while state
+                % Small steps
+                m = m + 0.1;
+                try
+                    [~] = zeros(nOmegas, m*10^n);
+                catch
+                    m = m - 0.1;
+                    state = false;
+                end
             end
+            
         end
-        
+    end
+    % Final number of elements we can ask for
+    nEl = floor(m * 10^n);
+    fprintf('Parallelizing factor %i, largest array needed %gGB\n', nEl, nEl*nOmegas*4/1e9);
+    
+    % Indexes for summation term
+    idx = 1;
+    if idx + nEl < N
+        edx = idx + nEl;
+    else
+        edx = N;
     end
 end
-nEl = floor(m * 10^n);
-fprintf('Parallelizing factor %i, largest array needed %gGB\n', nEl, nEl*N*4/1e9);
 %% Calculate the Fourier transform using eq. 10
 Y = 1i * omega * J0 ... % First term
     + ( 1 - exp(-1i * omega * tau(1)) ) * ( msd(1) - J0 ) / tau(1) ... % Second term
     + exp(-1i * omega * tau(end)) / eta; % Third term
+
 % Summation term
-idx = 1;
-if idx + nEl < N
-        edx = idx + nEl;
-    else
-        edx = N;
-end
 while idx <= N
     
-    fprintf('\r %i / %i', idx, N)
+    fprintf('\r %i:%i / %i', idx, edx, N)
     Y = Y + sum(( diff(msd(idx:edx))' ) ...
         .* ( exp(-1i * omega .* tau(idx:edx - 1)') - exp(-1i * omega .* tau(idx+1:edx)') ) ...
         ./ ( diff(tau(idx:edx))' ), 2);
