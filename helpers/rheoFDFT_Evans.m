1e9a053a585db6f6877794dcb517d1329eaf29a3
will hardiman <bill.hardiman1995@gmail.com>
2021-08-19 09:19:25 +0100

    Improved summation parallelisability

--- helpers/rheoFDFT_Evans.m
+++ helpers/rheoFDFT_Evans.m
@@ -1,4 +1,4 @@
-function [Y] = rheoFDFT_Evans(tau, msd, J0, eta)
+function [omega, Y] = rheoFDFT_Evans(tau, msd, nOmegas, J0, eta)
 %Y = rheoFDFT_Evans(tau, msd, J0, eta)
 %% Compute finite discrete fourier transform using Evans et al 2009
 % Requires consideration of the long-time limit of the gradient = 1/η
@@ -17,23 +17,25 @@ if tau(1) == 0 || min(msd(1,:) == 0)
     error('Expected non-zero lag time and MSDs')
 end
 
-% Setup
-omega = 2*pi./tau;
+% Setup: Frequency sampling
+omega = 2*pi./logspace(log10(tau(1)), log10(tau(end)), nOmegas)';
+% omega = 2*pi./linspace(tau(1), tau(end), nOmegas);
+% omega = 2*pi./tau;
 
 %% Calculate the Fourier transform using eq. 10
 Y = 1i * omega * J0 ... % First term
     + ( 1 - exp(-1i * omega * tau(1)) ) * ( msd(1) - J0 ) / tau(1) ... % Second term
     + exp(-1i * omega * tau(end)) / eta; % Third term
-% Summation term
-for k = 2:N
-    fprintf('\r%i / %i', k, N)
-    Y = Y + ( msd(k) - msd(k - 1) ) ...
-        .* ( exp(-1i * omega * tau(k - 1)) - exp(-1i * omega * tau(k)) ) ...
-        ./ ( tau(k) - tau(k - 1) );
+
+% Summation term - calculate summation in vectorized form, for each
+% frequency sequentially (embarassingly parallelizable)
+for wIdx = 1:nOmegas
+    Y(wIdx) = Y(wIdx) + sum( ( diff( msd ) ./ diff( tau ) ) ...
+        .* ( exp( -1i * omega(wIdx) * tau( 1:N-1 )) - exp( -1i * omega(wIdx) * tau( 2:N )) ));
 end
 
 Y = 1i .* omega ./ Y;
 
-fprintf('\rDone!\n')
+fprintf('\rDone!%s\n',repmat(' ', 50,1))
 
 end
