75a2558ffc7551f1e9987a3ffba846d78b32284f
will hardiman <bill.hardiman1995@gmail.com>
2021-08-20 09:23:20 +0100

    wrapper for rheo FT which interpolates first

---
+++ helpers/msd_interp_FT.m
@@ -0,0 +1,25 @@
+function [omega, G1, G2] = msd_interp_FT(tau, msd, g0, eta, nF, interpF, interpM)
+
+if ~exist('g0', 'var')
+    g0 = 0;
+end
+if ~exist('ginf', 'var')
+    eta = inf;
+end
+if ~exist('nF', 'var')
+    nF = length(tau);
+end
+if ~exist('interpF','var')
+    interpF = 1e3;
+end
+if ~exist('interpM','var')
+    interpM = 'pchip';
+end
+
+taui = logspace(log10(tau(1)), log10(tau(end)), length(tau).*interpF)';
+msdi = interp1(tau, msd, taui, interpM);
+
+[omega, Gv] = rheoFDFT_Evans_vec(taui, msdi, nF, g0, eta);
+
+G1 = real(Gv);
+G2 = imag(Gv);
