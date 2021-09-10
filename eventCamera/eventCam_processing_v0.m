3d3f79727d6cefe53824c9fd1a337adcdd6a58b7
will hardiman <bill.hardiman1995@gmail.com>
2021-09-10 09:39:17 +0100

    Wrote a new integration method/re-wrote one. Moved loads of stuff from eventCam_processing to eventCamera_dev

--- eventCamera/eventCam_processing_v0.m
+++ eventCamera/eventCam_processing_v0.m
@@ -10,7 +10,6 @@ addpath('~/junk/prophesee-matlab-scripts');
 evtCamDir = '~/mnt2/Users/billh/Event_Camera/';
 subDir = 'Event Camera - OpticallyTrappedBead/';
 % subDir = 'Event Camera - Stage_SineWave/';
-
 dirList = dir([evtCamDir subDir]);
 ls([evtCamDir subDir])
 
@@ -57,7 +56,7 @@ elseif strcmp(subDir(end-4:end),'Wave/')
         if length(dirList(fIdx).name) > 4
             if strcmp(dirList(fIdx).name(end-3:end), '.tif')
                 % just assume the data will always be sorted correctly
-%                 imStack{1}{fCount,1} = imread([evtCamDir subDir 'sCMOS_' fileBase '/' dirList(fIdx).name]);
+                imStack{1}{fCount,1} = imread([evtCamDir subDir 'sCMOS_' fileBase '/' dirList(fIdx).name]);
                 % This is at 0.108 um/px
                 fCount = fCount + 1;
             elseif strcmp(dirList(fIdx).name(end-3:end),'.txt')
@@ -116,36 +115,7 @@ scatter(centres(1,idxs), centres(2, idxs), [], imTs(idxs))
 xlim([50 57])
 axis equal
 title(sprintf('%.1fs to %.1fs', 1e-3.*imTs(idxs(1)), 1e-3.*imTs(idxs(end))))
-%% Load mat file with MSDs
-dirName = '~/Data/phd/EventCam/';
-fName = 'TrappedBead_3_td_msd.mat'; % MSD for set 1 doesn't exist
-% fName = 'Bead_Stage_SineWave_td_msd.mat';
-% fName = 'Fluorescence_Stage_SineWave_smallsteps_td_msd.mat';
-load([dirName fName],'msd')
-whos('msd')
-% xGauss = msd.tracks{1}(:,2);
-% yGauss = msd.tracks{2}(:,2);
-% tGauss = msd.tracks{1}(:,1);
-%% Plot loaded MSDs
-pow = 1;
-mult = 3e4;
-% Xrange = [-4.2 -2];
-Xrange = [-4.2 -1];
 
-figure(7)
-clf
-ax = gca;
-hold on
-msd.plotMSD
-ax.XAxis.Scale = 'log';
-ax.YAxis.Scale = 'log';
-
-X = logspace(Xrange(1), Xrange(2));
-Y = mult.*X.^pow;
-
-plot(X,Y,'--','LineWidth',2)
-
-legend('X', 'Y','α τ^1')
 %% Plot raw
 figure(1)
 clf 
@@ -200,30 +170,7 @@ elseif isfield(cdEvents,'p')
     xlabel('Polarity (+/-)')
 end
 
-% % ↓ This was when I was trying to load the raw files with the dat reader... 
-% %% Sort
-% eventsSorted = struct('ts',[]);
-% [eventsSorted.ts, I] = sort(cdEvents.ts);
-% eventsSorted.x = cdEvents.x(I);
-% eventsSorted.y = cdEvents.y(I);
-% eventsSorted.gray = cdEvents.gray(I);
-% %% Plot sorted
-% figure(1)
-% subplot(4,2,2)
-% plot(eventsSorted.ts,'.','MarkerSize', 1);
-% ylabel('Time (μs)')
-% 
-% subplot(4,2,4)
-% plot(eventsSorted.x,'.','MarkerSize', 1);
-% ylabel('X (px)')
-% 
-% subplot(4,2,6)
-% plot(eventsSorted.y,'.','MarkerSize', 1);
-% ylabel('Y (px)')
-% 
-% subplot(4,2,8)
-% plot(eventsSorted.gray,'.','MarkerSize', 1);
-% ylabel('Gray (arb. U)')
+
 %% Square windowing
 kernSz = 1000;
 
@@ -233,269 +180,99 @@ xWindowed = conv(cdEvents.x, kern, 'valid')./sum(kern);
 yWindowed = conv(cdEvents.y, kern, 'valid')./sum(kern);
 tWindowed = conv(cdEvents.ts, kern, 'valid')./sum(kern);
 
+
+% % Careful, this will eat all your rams :(
+% figure(2)
+% clf
+% scatter(xWindowed, yWindowed, [], tWindowed)
+
+ampEv = range(yWindowed);
+umPerPxEv = range(allPos(:,5))./ampEv; % 2.64 from stageSineWave
+
+stiffEV = calcStiffness([xWindowed, yWindowed]', 2.64e-6); % 2.64μm/px.
+% stiffEV = 1e-11 *[2.18; 4.41]; % For TrappedBead_1. Ratio X/Y = 0.493.
+%% And interpolate
+% How many events do you need per data point?
+%% SCMOS data
+thresh = 700;%2e4; % For trapped, 700? For sinewave, 2e4.
+crop = [31 90 31 90]; %[400 1200 700 1400]; % For sinewave. For trapped, [31 90 31 90].
+    
+if exist('imStack','var')
+    ims = cat(3, imStack{1}{:,1});
+    ims = ims(crop(3):crop(4), crop(1):crop(2), :);
+    %clear imStack
+
+elseif ~exist('ims','var')
+    error('Neither imStack nor ims exist as variables')
+end
+% % This is useful for determining the crop area
+% ims = max(ims, [], 3);
+% figure(1), clf, imagesc(ims)
+
+% %%
+% ims = ims(31:90, 31:90, :); % For trapped
+% centres = imCentreOfMass(double(ims).*(ims > thresh), 'simple') + [30; 30];
+
+centres = imCentreOfMass(double(ims).*(ims > thresh), 'simple') + crop(1:2)';
+stiffIms = calcStiffness(centres, 0.216e-6) % μm/px is different for trapped bead (2x2 binning)
+% stiffIms = 1e-7 * [8.37; 7.21]; % For TrappedBead_1. Ratio X/Y = 1.16.
+
+% figure(5)
+% clf
+% subplot(2,1,1)
+% scatter(centres(1,:), centres(2,:))
+% axis image
+% 
+% subplot(2,2,3)
+% histogram(centres(1,:))
+% xlabel('X (px)')
+% 
+% subplot(2,2,4)
+% histogram(centres(2,:))
+% xlabel('Y (px)')
+
+% Use y Range of centres to determine "height" (amplitude) of sine wave
+amp = 0.108.*range(centres(2,:)); % For sineWave sets, 0.108μm/px.
+% For sineWave, amp = 569.5284px=61.51μm, but from allPos, amp should be 600[μm??].
+%% Integrate events over window (refinement ROI)
+% Run the integration twice: Once with coarse time spacing and no ROI, and
+% once with fine time spacing and an ROI based on the coarse spacing.
+
+nt_coarse = 1e3;
+nt_fine = 1e4;
+
+[tsC, centresC] = integrateEvents(cdEvents.ts, cdEvents.x, cdEvents.y, nt_coarse);
+centresNoNaN = centresC(:,~isnan(centresC(1,:)));
+ROI = [mean(centresNoNaN,2)' range(centresNoNaN,2)'];
+[tsF, centresF] = integrateEvents(cdEvents.ts, cdEvents.x, cdEvents.y, nt_fine, ROI);
+centresNoNaN = centresF(:,~isnan(centresF(1,:)));
+stiffEVF = calcStiffness(centresNoNaN, 2.64e-6);
+% stiffEVF = 1e-12 * [1.938; 2.122]; % For TrappedBead_1. Ratio X/Y = 0.913.
+%% Integrate events over window (manual ROI)
+% Use an a priori ROI. You could use a single integration time to see where
+% to place this
+nt = 1e4;
+ROI = [300 230 40 60];
+[ts, centres] = integrateEvents(cdEvents.ts, cdEvents.x, cdEvents.y, nt, ROI);
 figure(2)
 clf
-scatter(xWindowed, yWindowed, [], tWindowed)
-%% Gaussian filtering
-sigma = 500;
+histogram(centres(1,:))
+centresNoNaN = centres(:,~isnan(centres(1,:)));
+stiffEVROI = calcStiffness(centresNoNaN, 2.64e-6);
+% stiffEVROI = 1e-11 * [3.35; 2.36]; % For TrappedBead_1. Ratio X/Y = 1.422.
 
-gaussSz = 2*ceil(2*sigma)+1;
-gauss = 1:gaussSz;
-gauss = exp(-(gauss-ceil(gaussSz/1)).^2./sigma.^2);
-
-% pGauss = conv(cdEvents.p, gauss, 'valid')./sum(gauss);
-idx = 1:numel(cdEvents.x);
-tmp = cdEvents.x(idx);
-xGauss = conv(tmp(cdEvents.p(idx) > 0), gauss, 'valid')./sum(gauss);
-% xGauss = conv(tmp, gauss, 'valid')./sum(gauss);
-tmp = cdEvents.y(idx);
-yGauss = conv(tmp(cdEvents.p(idx) > 0), gauss, 'valid')./sum(gauss);
-% yGauss = conv(tmp, gauss, 'valid')./sum(gauss);
-tmp = cdEvents.ts(idx);
-tGauss = conv(tmp(cdEvents.p(idx) > 0), gauss, 'valid')./sum(gauss);
-% tGauss = conv(tmp, gauss, 'valid')./sum(gauss);
-tGauss = tGauss - min(tGauss);
-clear tmp
-% xGauss = conv(cdEvents.x(1:4:end), gauss, 'valid')./sum(gauss);
-% yGauss = conv(cdEvents.y(1:4:end), gauss, 'valid')./sum(gauss);
-% tGauss = conv(cdEvents.ts(1:4:end), gauss, 'valid')./sum(gauss);
-%
-figure(3)
-clf
-hold on
-scatter(xGauss, yGauss, [], tGauss./1e3)
-% scatter(200+allPos(:,2)./3, 225-allPos(:,3)./3, [], allPos(:,1))
-title('Bead location from event camera')
-%% Animate
-step = 1e3;
-overlap = 5e3;
-nSteps = 200;
-figure(4)
-clf
-hold on
-ax = gca;
-% scatter(200+allPos(:,2)./3, 225-allPos(:,3)./3)%, [], tGauss(end)*allPos(:,1)./allPos(1))
-for idx = 1:nSteps
-    scatter(xGauss((idx-1)*step + (1:(step+2*overlap))), ...
-        yGauss((idx-1)*step + (1:(step+2*overlap))), ...
-        [], ...30*nGauss((idx-1)*step + (1:(step+2*overlap)))/max(nGauss) , ...
-        tGauss((idx-1)*step + (1:(step+2*overlap))))
-    title(sprintf('%.1fs to %.1fs',1e-6*tGauss((idx-1)*step + 1),1e-6*tGauss((idx-1)*step + step)))
-    xlim([100 400])
-    ylim([100 350])
-    pause(0.15)
-    if idx < nSteps
-        delete(ax.Children(1))
-    end
-end
-%%
-figure(99) % I got 99 figures but...
-clf
-ax2 = subplot(2,1,2);
-ax1 = subplot(2,1,1);
-for idx = 1:10
-%     title(ax1, sprintf('%i events', (step+2*overlap)))
-    histogram(ax1, xGauss((idx-1)*step + (1:(step+2*overlap))))
-    histogram(ax2, yGauss((idx-1)*step + (1:(step+2*overlap))))
-    drawnow
-    pause(0.25)
-end
-%% Integrate some
-map = [0 10 0
-    0 9 0
-    0 8 0
-    0 7 0 
-    0 6 0
-    0 5 0
-    5 5 5
-    5 0 0
-    6 0 0
-    7 0 0 
-    8 0 0
-    9 0 0
-    10 0 0]/10;
-idxs = 3896:72712;
-figure(10)
-plot(idxs, cdEvents.ts(idxs))
-xlabel('Event number')
-ylabel('Time stamp (μs)')
-title('Time distribution of events')
-
-im = zeros(max(cdEvents.y)+1,max(cdEvents.x)+1);
-for idx = idxs
-    x = cdEvents.x(idx)+1;
-    y = cdEvents.y(idx)+1;
-    im(y, x) = im(y, x) + cdEvents.p(idx);
-end
-% im = 1.2.^im;
-% im = log(im);
-figure(11)    
-imagesc(im)
-colorbar
-caxis([-1 1] * 20)
-xlim([160 240])
-ylim([190 260])
-title(sprintf('Sum of events from %.2gs to %.2gs',cdEvents.ts(idxs(1))*1e-6,cdEvents.ts(idxs(end))*1e-6))
-%% Gaussian weighted Gaussian filtering
-sigma = 500;
-
-gaussSz = 2*ceil(2*sigma)+1;
-gauss = 1:gaussSz;
-gauss = exp(-(gauss-ceil(gaussSz/1)).^2./sigma.^2)/(sigma*sqrt(2*pi));
-
-% pGauss = conv(cdEvents.p, gauss, 'valid')./sum(gauss);
-idx = 1:numel(cdEvents.x)/1024;
-tmp = cdEvents.x(idx);
-% xGauss = conv(tmp(cdEvents.p(idx) > 0), gauss, 'valid')./sum(gauss);
-xGauss = conv(tmp, gauss, 'valid')./sum(gauss);
-tmp = cdEvents.y(idx);
-% yGauss = conv(tmp(cdEvents.p(idx) > 0), gauss, 'valid')./sum(gauss);
-yGauss = conv(tmp, gauss, 'valid')./sum(gauss);
-tmp = cdEvents.ts(idx);
-% tGauss = conv(tmp(cdEvents.p(idx) > 0), gauss, 'valid')./sum(gauss);
-tGauss = conv(tmp, gauss, 'valid')./sum(gauss);
-
-
-
-%% MSD???
-% tCrop = cdEvents.ts(ceil(gaussSz/2):ceil(444024-gaussSz/2));
-tracks = {[tGauss, xGauss], [tGauss, yGauss]};
-
-msd = msdanalyzer(1, 'px', 'us','original');
+%% MSD for refinement method
+msd = msdanalyzer(1, 'px', 's', 'log');
+tracks = {[tsF', centresF(1,:)'], [tsF', centresF(2,:)']};
 msd = msd.addAll(tracks);
 msd = msd.computeMSD;
 
-figure(4)
-clf
-msd.plotMSD;
-ax = gca;
-ax.XAxis.Scale = 'log';
-ax.YAxis.Scale = 'log';
-%% 
-% save(msd,'eventcam_msd.mat')
-%% SCMOS data
-thresh = 700;
-
-ims = cat(3, imStack{1}{:,1});
-ims = ims(31:90, 31:90, :);
-centres = imCentreOfMass(double(ims).*(ims > thresh), 'simple') + [30; 30];
-
-figure(5)
-clf
-subplot(2,1,1)
-scatter(centres(1,:), centres(2,:), [], ts)
-axis image
-
-subplot(2,2,3)
-histogram(centres(1,:))
-xlabel('X (px)')
-
-subplot(2,2,4)
-histogram(centres(2,:))
-xlabel('Y (px)')
-%% Regularize time data for reasons
-nTimes = 1e5;
-
-sigma = 5*range(cdEvents.ts)./nTimes;
-
-gaussWdth = 2*ceil(2*sigma)+1;
-
-nEvents = numel(cdEvents.ts);
-dT = floor(range(cdEvents.ts)/nTimes);
-
-times = round(0:dT:(max(cdEvents.ts)-dT-1))+min(cdEvents.ts);
-times2 = round(0:dT:(max(cdEvents.ts)-dT-1))+min(cdEvents.ts)+gaussWdth;
-
-tIdxs = zeros(length(times),1);
-tIdxs2 = zeros(length(times),1);
-
-residuals = zeros(length(times),1);
-residuals2 = zeros(length(times),1);
-%% Get indexes for raw data
-%     % create matrix of abs differences, find minimum  to choose as index
-%     % take those indexes out of xGauss yGauss (tGauss??) and MSD with log
-%     % spacing
-
-idx = 0;
-idxs = 1:2e4;
-
-while idx < length(times)
-    idx = idx + 1;
-
-    % You can go your own waaaayyy
-    [residuals(idx), tIdxs(idx)] = min(abs(cdEvents.ts(idxs) - times(idx)));
-
-    % Needs better check for idxs going beyond max
-    while idxs(end) < nEvents  && residuals(idx) == abs(cdEvents.ts(idxs(end)) - times(idx))
-        idxs = idxs + min(15e3,nEvents-idxs(end));
-        [residuals(idx), tIdxs(idx)] = min(abs(cdEvents.ts(idxs) - times(idx)));
-    end
-    tIdxs(idx) = tIdxs(idx) + idxs(1) - 1;
-end
-disp('timed')
-idx = 0;
-idxs = 1:2e4;
-while idx < length(times2)
-    idx = idx + 1;
-    
-    % You can go your own waaaayyy
-    [residuals2(idx), tIdxs2(idx)] = min(abs(cdEvents.ts(idxs) - times2(idx)));
-
-    while idxs(end) < nEvents && residuals2(idx) == abs(cdEvents.ts(idxs(end)) - times2(idx))
-        idxs = idxs + min(15e3,nEvents-idxs(end));
-        [residuals2(idx), tIdxs2(idx)] = min(abs(cdEvents.ts(idxs) - times2(idx)));
-    end
-    tIdxs2(idx) = tIdxs2(idx) + idxs(1) - 1;
-end
-disp('time2''d')
-%% Group events and apply Gaussian
-eventsGrouped = cell(length(tIdxs),1);
-for idx = 1:length(tIdxs)
-    idxs = tIdxs(idx):tIdxs2(idx);
-%     eventsGrouped{idx} = [cdEvents.ts(idxs) cdEvents.x(idxs) cdEvents.y(idxs) cdEvents.p(idxs)];
-    eventsGrouped{idx} = [cdEvents.ts(idxs) cdEvents.x(idxs) cdEvents.y(idxs)];
-end
-disp('grouped')
-% Apply filter in parallel
-xGauss = zeros(length(tIdxs),1);
-yGauss = zeros(length(tIdxs),1);
-tGauss = zeros(length(tIdxs),1);
-nGauss = tIdxs2 - tIdxs;
-
-parfor idx = 1:length(tIdxs)
-    data = eventsGrouped{idx};
-    
-    muT = (times2(idx) + times(idx))*0.5;
-    gaussT = (1/sqrt(2*pi*sigma.^2)) * exp(- (data(:,1) - muT).^2/sigma.^2);
-    normFactor = sum(gaussT);
-    
-    tGauss(idx) = sum(data(:,1).*gaussT)./normFactor;
-    xGauss(idx) = sum(data(:,2).*gaussT)./normFactor;
-    yGauss(idx) = sum(data(:,3).*gaussT)./normFactor;
-end
-clear eventsGrouped
-disp('done!')
-%%
-xSF = (337 - 141)/range(allPos(:,2));
-ySF = (324 - 115)/range(allPos(:,3));
-xOS = 210;
-yOS = 225;
-%
-figure(6)
+figure(8)
 clf
 hold on
-scatter(xGauss, yGauss, 30*nGauss/max(nGauss), 0.5*(times+times2))
-% scatter(xGauss, yGauss, [], tGauss)
-scatter(xOS+allPos(:,2)*xSF, yOS-allPos(:,3)*ySF,[],'k','.')
-legend('Event data','Stage position')
-xlabel('X (px)')
-ylabel('Y (px)')
-title(sprintf('Regularized reconstructed data, gaussian σ = %0.1fms',1e-3*sigma))
-%% Event distribution in time
-idx = 3e5;
-figure(7)
-histogram(1e-6*cdEvents.ts(1e5:idx),100)
-xlabel('Event time (s)')
-ylabel('Count')
-title(sprintf('Time distribution of first %.G events',idx))
+ax = gca;
+ax.XScale = 'log';
+ax.YScale = 'log';
 
+plot(msd.msd{1}(:,1), msd.msd{1}(:,2))
+plot(msd.msd{1}(:,1), msd.msd{2}(:,2))
