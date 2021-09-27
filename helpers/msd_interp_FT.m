a6ce0fd404416ed08b3e95a030b78ec6bc873abe
beeeel <bill.hardiman1995@gmail.com>
2021-09-27 10:58:10 +0100

    improved doc

--- helpers/msd_interp_FT.m
+++ helpers/msd_interp_FT.m
@@ -1,4 +1,8 @@
 function [omega, G1, G2] = msd_interp_FT(tau, msd, g0, eta, nF, interpF, interpM)
+%% Calculate rheological Fourier Transform of msd sampled at times tau.
+% Use zero-time point g0, long time inverse gradient eta, calculate at nF
+% different frequencies, interpolate msd by factor interpF using method
+% interpM.
 
 if ~exist('g0', 'var')
     g0 = 0;
