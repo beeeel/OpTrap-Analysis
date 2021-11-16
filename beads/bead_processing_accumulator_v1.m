c5404212328d7de7c2a2976e2c377a85cc9fc24b
will hardiman <bill.hardiman1995@gmail.com>
2021-11-16 19:21:16 +0000

    lots. Includes ToDo list, better handling of corner finding, better saving and loading, and more complete lists of wRange and tRange

--- beads/bead_processing_accumulator_v1.m
+++ beads/bead_processing_accumulator_v1.m
@@ -6,6 +6,17 @@
 %       filtering before recalculating MSD and FTs.
 %
 % This file accumulates: Latrunculin B data
+%
+%% TO DO:
+%
+% Better warnings when no corners: Filename and which corner
+% Quieter warnings when plotting etc - there's always negative Y
+% Finding α, D from MSDs
+% Store α,D from fitting to tRanges - for high static error, is D constant?
+% Storing MSD(1./fpass)
+% Better MSD field handling - need to use centresHP when possible and
+%  centresM otherwise
+% data.opts.nSkip - use this in any function which handles MSDs
 
 %% Data parameters
 close all
@@ -66,17 +77,19 @@ stiffIdx = 1;           % Row of centres to store stiffness from
 fpass = 0;              % Pass frequency
 cropTHPval = 0;         % Frames to crop after HP filter
 msdOffset = [1];        % Offset from start when taking data to calculate mean-square displacements
+loadPics = false;       % Load images from TIFs. May be overriden by angleCorrection
 
 msdDim = 'all';         % Direction to calculate MSD in - 'x', 'y', or 'all'
-msdCentresRow = 1;      % Row of centres array to use for MSDs (empty for default)
+centresRow = 1;      % Row of centres array to use for MSDs (empty for default)
 msdNumT = [];           % Number of time points to use for MSDs (empty for all)
 msdUseRaw = true;      % Use raw or processed data for MSDs (empty for default)
 msdDoNorm = true;       % Normalize MSDs by position variance (empty for default)
 doFFT = false;           % Calculate FFT and maybe plot
 
 % Data file parameters
-forceRun = true;       % Try to take data from file and reuse as much as possible
-saveData = false;        % Save data to file
+forceRun = false;       % Try to take data from file and reuse as much as possible
+saveDataPro = false;    % Save processed data to file (probably only makes things slower for now)
+saveDataRaw = true;     % Save raw data to file (should speed up loading)
 saveAccu = true;        % Save data to file
 dataSuff = '_simple';       % Suffix for filename when saving/loading
 accuFile = 'accumulated_3days';
@@ -91,6 +104,7 @@ fprintf('Working in %s\n',pwd)
 fileCount = 1;
 %%
 fh = figure(69);
+fh2 = figure(99);
 % For each day chosen
 for dayIdx = 1:length(dayDirs)
     % Get the correct directory
@@ -128,12 +142,13 @@ for dayIdx = 1:length(dayDirs)
                 % Set names and load data
                 data.dirPath = [dirList(fileIdx).folder '/' dirList(fileIdx).name];
                 data.fName = dirList(fileIdx).name;
-                data = bead_loadData(data, angleCorrection);
+                data = bead_loadData(data, any([loadPics, angleCorrection]));
+                fprintf('\n\t\t Loaded:\t\t%s\n\n',data.fName)
                 
-                % Apply calibration and crop time
-                data.opts.cropT = cropTs{1};
-                data.opts.pOrder = fitPolyOrder*fitPoly;
-                data.mPerPx = mPerPx;
+                % Save short data if requested
+                if saveDataRaw
+                    save(dataFile, 'data')
+                end
             else
                 tmp = load(dataFile, 'data');
                 data = tmp.data;
@@ -141,10 +156,12 @@ for dayIdx = 1:length(dayDirs)
                 data.opts.forceRun = forceRun;
             end
 
+            % Record options and calibration
+            data.opts.cropT = cropTs{1};
+            data.opts.pOrder = fitPolyOrder*fitPoly;
+            data.mPerPx = mPerPx;
             data.opts.angleCorrection = angleCorrection;
-
-            % Store filenames
-            accumulated{dayIdx}{1,cellIdx}(fIdx).fName = data.fName;
+            data.opts.centresRow = centresRow;
 
             % Apply scale and do polyfit
             data = bead_preProcessCentres(data);
@@ -160,8 +177,8 @@ for dayIdx = 1:length(dayDirs)
 
             % Look at mean-square displacement (for cell-bead expts)
             if ~isempty(msdOffset)
-                data = bead_normMSD_polyfit(data, msdDim, msdOffset, msdNumT, false, msdUseRaw, msdCentresRow, msdDoNorm);
-                accumulated{dayIdx}{1,cellIdx}(fIdx).msd = data.pro.amsdObj;
+                data = bead_normMSD_polyfit(data, msdDim, msdOffset, msdNumT, false, msdUseRaw, centresRow, false);
+%                 accumulated{dayIdx}{1,cellIdx}(fIdx).msd = data.pro.amsdObj;
             end
             
             % Get wRange from struct
@@ -170,39 +187,79 @@ for dayIdx = 1:length(dayDirs)
             % Do the fourier transform to find intercept frequency
             [FT, oC] = msd_fourier_transformator(data.pro.amsdObj, accumulated{dayIdx}{2,cellIdx}(fIdx), ...
                 'wRange',wR, 'fh', fh);
+            oC = cat(1,oC{:});
             
             % Get tRange from struct
             tR = Range_getter(tRanges, dayDirs{dayIdx}, cellIdx, fIdx);
             % Also fit to the MSD to find corner time equivalent frequency
             tC = msd_cornerator(data.pro.amsdObj, accumulated{dayIdx}{2,cellIdx}(fIdx), tR);
             
+            if any( isnan( tC ) & isnan( oC' ) )
+                tr = {'tangential','radial'};
+                if sum( isnan( tC ) & isnan( oC' ) ) == 1
+                    % Could give name of file in warning
+                    warning('No corner in %s direction, highpass only using', tr{ sum( isnan( tC ) & isnan( oC' ) ) })
+                else
+                    warning('No corner in either direction, skipping highpass.')
+                end
+                % Could give better warnings:
+%             else
+%                 if any( isnan( tC ) )
+%                     warning('Got NaN back from msd_cornerator. Highpass only using corner frequency')
+%                 end
+%                 if any( isnan( oC ) )
+%                     warning('Got NaN back from msd_fourier_transformator. Highpass only using corner time')
+%                 end
+            end
             
-            % Probably take the mean of the two for pass frequency
-            fpass = mean([tC 1./oC]); % ( Double check sizes here )
-            % Also warning about tC might not have everything as expected -
-            % e.g. tangential plateau at long times means no late uptick
+%             % Do something with the corners
+%             accumulated{dayIdx}{1,cellIdx}(fIdx).corners = 
+            
+            % Use mean of 1/corner time and intercept frequency. There's
+            % definitely scope for mistakes to occur if you want to be
+            % getting smarter, but that will have to wait. 
+            fpass = mean([1./tC; oC'],'omitnan');
+            
+            % This has assumed you've only given one intercept frequency
+            % range and one corner range
             
             % High-pass filter and calculate Allan variance if pass frequency is positive
-            if fpass > 0
+            if any(fpass > 0)
                 data.opts.fpass = fpass;
                 data.opts.cropTHPval = cropTHPval;
                 % Need to adapt to run on both dims and use subplots/etc
-                data = bead_hp_allan_var(data, 'xCentresPx', ...
-                    fpass, cropTHPval, false);
-                data = bead_hp_allan_var(data, 'yCentresPx', ...
-                    fpass, cropTHPval, false);
+                if fpass(1) > 0
+                    data = bead_hp_allan_var(data, 'xCentresM', ...
+                        fpass(1), cropTHPval, false);
+                    xStiff = calcStiffness(data.pro.xCentresHP);
+                else
+                    xStiff = calcStiffness(data.pro.xCentresM(centresRow,:));
+                end
                 
-                xStiff = calcStiffness(data.pro.xCentresHP);
-                yStiff = calcStiffness(data.pro.yCentresHP);
-                data.pro.stiffXYHP = [xStiff, yStiff];
-                accumulated{dayIdx}{1,cellIdx}(fIdx).stiffHP = data.pro.stiffXYHP;
+                if fpass(2) > 0
+                    data = bead_hp_allan_var(data, 'yCentresM', ...
+                        fpass(2), cropTHPval, false);
+                    yStiff = calcStiffness(data.pro.yCentresHP);
+                else
+                    yStiff = calcStiffness(data.pro.yCentresM(centresRow,:));
+                end
                 
+                data.pro.stiffXY = [xStiff, yStiff];
+                accumulated{dayIdx}{1,cellIdx}(fIdx).stiff = data.pro.stiffXY;
+                accumulated{dayIdx}{1,cellIdx}(fIdx).fpass = fpass;
+                accumulated{dayIdx}{1,cellIdx}(fIdx).opts = data.opts;
+                
+                data = bead_normMSD_polyfit(data, msdDim, msdOffset, msdNumT, true, false, centresRow, false, false, 'centresHP');
+                accumulated{dayIdx}{1,cellIdx}(fIdx).msd = data.pro.amsdObj;
+            else
+                xStiff = calcStiffness(data.pro.xCentresM(1,:));
+                yStiff = calcStiffness(data.pro.yCentresM(1,:));
+                data.pro.stiffXY = [xStiff, yStiff];
+                accumulated{dayIdx}{1,cellIdx}(fIdx).stiff = data.pro.stiffXY;
             end
             
-            
-            
             % Save if requested
-            if saveData
+            if saveDataPro
                 save(dataFile, 'data')
             end
             
@@ -240,34 +297,35 @@ function [wRanges] = get_wRanges_struct(dayDirs)
 % This has numbers from LatB data
 wRanges = struct(strcat('d',dayDirs{end}), struct('c1', {{}}));
 
+%% wRanges with 1 low-frequency corner
 wRanges.d2021_07_27.c2 = {...
-    {[2e-2 1] [5 1e2]} {[2e-2 1] [5 1e2]};
-    {[2e-2 1] [5 1e2]} {[2e-2 1] [5 1e2]};
-    {[2e-2 0.8] [8e2 2e3]} {[8 80]};
-    {[2e-2 0.8] [8e2 2e3]} {[8 80]};
+    {[2e-2 1]} {[2e-2 1]};
+    {[2e-2 1]} {[2e-2 1]};
+    {[2e-2 0.8]} {[8 80]};
+    {[2e-2 0.8]} {[8 80]};
     {[2 10]} {[2 10]};
     {[2 10]} {[2 10]};
     {[1 7]} {[1 10]};
-    {[1e-2 1.5e-1] [30 1e4]} {[2 10]};};
+    {[1e-2 1.5e-1]} {[2 10]};};
 wRanges.d2021_07_27.c1 = {...
-    {[1e-2 2] [200 2e3]} {[1e-2 5] [60 8e2]};
-    {[1e-2 5] [500 5e4]} {[1e-2 6] [200 1e4]};
-    {[1e-2 2] [8e2 2e3]} {[1e-2 1] [200 1e4]};
-    {[1e-2 2] [8e2 2e3]} {[1e-1 2] [200 1e4]};
-    {[1e-2 1] [8e2 2e3]} {[1e-2 1] [200 1e4]};
-    {[1e-1 3] [20 1e2]} {[1e-2 1] [200 1e4]};
-    {[1e-2 1] [6e2 2e3]} {[1e-2 1] [200 1e4]};
-    {[1e-2 1] [6e2 2e3]} {[1e-2 1] [200 1e4]};
-    {[1e-2 1] [6e2 2e3]} {[1e-2 1] [200 1e4]}};
+    {[1e-2 2]} {[1e-2 5]};
+    {[1e-2 5]} {[1e-2 6]};
+    {[1e-2 2]} {[1e-2 1]};
+    {[1e-2 2]} {[1e-1 2]};
+    {[1e-2 1]} {[1e-2 1]};
+    {[1e-1 3]} {[1e-2 1]};
+    {[1e-2 1]} {[1e-2 1]};
+    {[1e-2 1]} {[1e-2 1]};
+    {[1e-2 1]} {[1e-2 1]}};
 wRanges.d2021_07_26.c1 = {...
-    {[1e-2 10] [50 3e2]} {[1e-2 10] [20 8e2]};
-    {[1e-2 5] [50 5e2]} {[1e-2 6] [20 8e2]};
-    {[1e-2 10] [50 3e2]} {[1e-2 10] [20 8e2]};
-    {[6e-2 2] [50 3e2]} {[6e-2 2] [20 8e2]};
-    {[1e-2 1] [50 3e2]} {[1e-2 1] [20 8e2]};
-    {[1e-1 3] [20 1e2]} {[1e-2 3] [20 2e2]};
-    {[1e-1 3] [50 3e2]} {[1e-2 3] [50 3e2]};
-    {[1e-1 3] [10 2e2]} {[1e-2 3] [10 2e2]}};
+    {[1e-2 10]} {[1e-2 10]};
+    {[1e-2 5]} {[1e-2 6]};
+    {[1e-2 10]} {[1e-2 10]};
+    {[6e-2 2]} {[6e-2 2]};
+    {[1e-2 1]} {[1e-2 1]};
+    {[1e-1 3]} {[1e-2 3]};
+    {[1e-1 3]} {[1e-2 3]};
+    {[1e-1 3]} {[1e-2 3]}};
 wRanges.d2021_07_23.c3 = {...
     {[100 1e4]} {[20 8e2]};
     {[100 1e4]} {[20 8e2]};
@@ -275,11 +333,11 @@ wRanges.d2021_07_23.c3 = {...
     {[100 1e4]} {[20 8e2]};
     {[100 1e4]} {[20 8e2]};
     {[100 1e4]} {[20 8e2]};
-    {[1e-2 0.8] [100 1e4]} {[100 1e4]};
+    {[1e-2 0.8]} {[100 1e4]};
     {[100 1e4]} {[20 8e2]};
     {[100 1e4]} {[20 1e3]}};
 wRanges.d2021_07_23.c2 = {...
-    {[1e-2 1] [70 1e4]} {[5 80]};
+    {[1e-2 1]} {[5 80]};
     {[3 50]} {[2 30]};
     {[3 50]} {[0.5 5]};
     {[3 50]} {[0.5 5]};
@@ -298,8 +356,70 @@ wRanges.d2021_07_23.c1 = {...
     {[1e-1 5]} {[1e-1 5]};
     {[1e-2 2]} {[1e-2 2]}; %
     {[1e-2 30]} {[1e-2 8]};
-    {[1e-2 1] [20 200] [300 1e3]} {[1e-2 2]};
-    {[1e-2 1] [20 200] [300 1e3]} {[1e-2 2]}};
+    {[1e-2 1]} {[1e-2 2]};
+    {[1e-2 1]} {[1e-2 2]}};
+
+%% wRanges with 2 corners
+% wRanges.d2021_07_27.c2 = {...
+%     {[2e-2 1] [5 1e2]} {[2e-2 1] [5 1e2]};
+%     {[2e-2 1] [5 1e2]} {[2e-2 1] [5 1e2]};
+%     {[2e-2 0.8] [8e2 2e3]} {[8 80] []};
+%     {[2e-2 0.8] [8e2 2e3]} {[8 80] []};
+%     {[2 10]} {[2 10]};
+%     {[2 10]} {[2 10]};
+%     {[1 7]} {[1 10]};
+%     {[1e-2 1.5e-1] [30 1e4]} {[2 10]};};
+% wRanges.d2021_07_27.c1 = {...
+%     {[1e-2 2] [200 2e3]} {[1e-2 5] [60 8e2]};
+%     {[1e-2 5] [500 5e4]} {[1e-2 6] [200 1e4]};
+%     {[1e-2 2] [8e2 2e3]} {[1e-2 1] [200 1e4]};
+%     {[1e-2 2] [8e2 2e3]} {[1e-1 2] [200 1e4]};
+%     {[1e-2 1] [8e2 2e3]} {[1e-2 1] [200 1e4]};
+%     {[1e-1 3] [20 1e2]} {[1e-2 1] [200 1e4]};
+%     {[1e-2 1] [6e2 2e3]} {[1e-2 1] [200 1e4]};
+%     {[1e-2 1] [6e2 2e3]} {[1e-2 1] [200 1e4]};
+%     {[1e-2 1] [6e2 2e3]} {[1e-2 1] [200 1e4]}};
+% wRanges.d2021_07_26.c1 = {...
+%     {[1e-2 10] [50 3e2]} {[1e-2 10] [20 8e2]};
+%     {[1e-2 5] [50 5e2]} {[1e-2 6] [20 8e2]};
+%     {[1e-2 10] [50 3e2]} {[1e-2 10] [20 8e2]};
+%     {[6e-2 2] [50 3e2]} {[6e-2 2] [20 8e2]};
+%     {[1e-2 1] [50 3e2]} {[1e-2 1] [20 8e2]};
+%     {[1e-1 3] [20 1e2]} {[1e-2 3] [20 2e2]};
+%     {[1e-1 3] [50 3e2]} {[1e-2 3] [50 3e2]};
+%     {[1e-1 3] [10 2e2]} {[1e-2 3] [10 2e2]}};
+% wRanges.d2021_07_23.c3 = {...
+%     {[100 1e4]} {[20 8e2]};
+%     {[100 1e4]} {[20 8e2]};
+%     {[100 1e4]} {[20 8e2]};
+%     {[100 1e4]} {[20 8e2]};
+%     {[100 1e4]} {[20 8e2]};
+%     {[100 1e4]} {[20 8e2]};
+%     {[1e-2 0.8] [100 1e4]} {[100 1e4]};
+%     {[100 1e4]} {[20 8e2]};
+%     {[100 1e4]} {[20 1e3]}};
+% wRanges.d2021_07_23.c2 = {...
+%     {[1e-2 1] [70 1e4]} {[5 80]};
+%     {[3 50]} {[2 30]};
+%     {[3 50]} {[0.5 5]};
+%     {[3 50]} {[0.5 5]};
+%     {[0.5 4]} {[0.5 4]};
+%     {[3e-1 5]} {[3e-1 3]};
+%     {[1 10]} {[1 10]}; %
+%     {[2e-1 10]} {[2e-1 8]};
+%     {[4e-1 3]} {[1e-1 3]};
+%     {[1 5]} {[1 5]};};
+% wRanges.d2021_07_23.c1 = {...
+%     {[1e-1 5]} {[1e-1 5]};
+%     {[5e-1 500]} {[1e-1 5]};
+%     {[1e-1 5]} {[1e-1 5]};
+%     {[1e-1 3]} {[1e-1 3]};
+%     {[1e-1 3]} {[1e-1 3]};
+%     {[1e-1 5]} {[1e-1 5]};
+%     {[1e-2 2]} {[1e-2 2]}; %
+%     {[1e-2 30]} {[1e-2 8]};
+%     {[1e-2 1] [20 200] [300 1e3]} {[1e-2 2]};
+%     {[1e-2 1] [20 200] [300 1e3]} {[1e-2 2]}};
 
 end
 
@@ -307,16 +427,134 @@ function [tRanges] = get_tRanges_struct(dayDirs)
 % This has numbers from LatB data
 tRanges = struct(strcat('d',dayDirs{end}), struct('c1', {{}}));
 
-tRanges.d2021_07_27.c2 = {...
-    };
-tRanges.d2021_07_27.c1 = {...
-    };
-tRanges.d2021_07_26.c1 = {...
-    };
+%% Long time corners
+
+tRanges.d2021_05_17.c2 = {
+    {[1e-2 0.5] [4 20]} {[1e-2 1] [4 20]}
+    {[1e-2 0.5] [4 20]} {[1e-2 1] [4 20]}
+    {[1e-2 0.5] [4 20]} {[1e-2 1] [4 20]}
+    {[1e-2 0.5] [4 20]} {[1e-2 1] [4 20]}
+    {[1e-2 0.5] [3 20]} {[1e-2 1] [4 20]}
+    {[1e-2 0.5] [3 20]} {[1e-2 1] [4 20]}};
+
+tRanges.d2021_05_17.c1 = {
+    {} {}
+    {} {}
+    {[1e-1 1] [6 20]} {}
+    {[1e-1 1] [6 20]} {[1.4e-2 1] [5 20]}};
+
+tRanges.d2021_07_16.c1 = {
+    {} {}
+    {[3e-1 3] [10 20]} {[3e-1 2] [10 20]}
+    {[2e-1 3/5] [10 20]} {[3e-1 2] [10 20]}
+    {[2e-2 1] [5 20]} {[2e-2 1] [10 20]}
+    {[2e-2 0.25] [5 20]} {[2e-2 1] [10 20]}
+    {[2e-2 0.25] [5 20]} {[2e-2 1] [10 20]}
+    {[2e-2 0.25] [5 20]} {}};
+
+tRanges.d2021_07_13.c1 = {...
+    {[1e-2 2e-1] [5 20]} {[1e-2 2e-1] [5 20]}
+    {[1e-2 2e-1] [5 20]} {[1e-2 2e-1] [5 20]}
+    {[1e-2 2e-1] [5 20]} {[1e-2 2e-1] [5 20]}
+    {[1e-2 2e-1] [5 20]} {[1e-2 2e-1] [5 20]}
+    {[1e-2 2e-1] [5 20]} {[1e-2 2e-1] [5 20]}
+    {[1e-2 2e-1] [5 20]} {[1e-2 2e-1] [5 20]}
+    {[1e-2 2e-1] [5 20]} {}
+    {[1e-2 2e-1] [5 20]} {[1e-2 2e-1] [5 20]}
+    {[1e-2 2e-1] [5 9]} {[1e-2 2e-1] [5 20]}
+    {[1e-2 2e-1] [5 9]} {[1e-2 2e-1] [5 20]}
+    {[1e-2 2e-1] [5 20]} {}
+    {[1e-2 2e-1] [5 20]} {}};
+
 tRanges.d2021_07_23.c3 = {...
-    };
+    {[1e-2 1] [10 30]} {}
+    {[1e-2 1] [10 30]} {[1e-1 2] [10 30]}
+    {[1e-2 1] [10 30]} {[1e-1 2] [10 30]}
+    {[1e-2 1] [10 30]} {[1e-1 2] [10 30]}
+    {[1e-2 1] [10 30]} {[1e-1 2] [10 30]}
+    {[1e-2 1] [10 30]} {}
+    {[1e-2 1] [10 30]} {}
+    {[1e-2 1] [5 30]} {[1e-1 2] [5 30]}
+    {[1e-2 1] [10 30]} {}};
 tRanges.d2021_07_23.c2 = {...
-    };
+    { } { }
+    { } { }
+    { } { }
+    { } { }
+    { } { }
+    { } { }
+    { } { }
+    { } { }
+    { } { }
+    { } { }};
+
 tRanges.d2021_07_23.c1 = {...
-    };
+    { } { }
+    { } { }
+    { } { }
+    { } { }
+    { } { }
+    { } { }
+    { } { }
+    { } { }
+    { } { }
+    { } { }};
+
+tRanges.d2021_07_26.c1 = {...
+    {[0.08 0.3] [3 20]} {[0.05 0.3] [3 20]}
+    {[0.08 0.3] [3 10]} {[0.05 0.3] [3 10]}
+    {[0.08 0.3] [3 20]} {[0.05 0.3] [3 20]}
+    {[0.1 0.8] [5 30]} {[0.1 0.8] [5 30]}
+    {[0.1 0.8] [5 30]} {[0.1 0.8] [5 30]}
+    {[0.1 0.8] [5 15]} {[0.1 0.8] [5 30]}
+    {[0.1 0.8] [5 15]} {[0.1 0.8] [5 30]}
+    {[0.2 1] [3 15]} {[0.2 0.8] [7 30]}};
+
+tRanges.d2021_07_27.c2 = {...
+    {[0.2 3] [20 50]} {[0.1 1] [20 50]}
+    {[0.4 1.4] [8 20]} {[0.4 1.4] [8 20]}
+    {[0.4 2] [8 40]} { }
+    {[0.4 4] [15 40]} { }
+    { } { }
+    { } { }
+    { } { }
+    {[5 20] [70 200]} { }};
+
+tRanges.d2021_07_27.c1 = {...
+    {[3e-2 1/2] [2 7]} {[3e-2 1/2] [2 7]}
+    {[1e-2 0.1] [4 20]} {[1e-2 0.4] [5 20]}
+    {[1e-2 0.1] [4 20]} {[1e-2 0.4] [5 20]}
+    {[1e-2 0.1] [8 30]} {[1e-2 0.4] [8 30]}
+    {[1e-2 1] [7 30]} {[1e-2 1] [7 30]}
+    {[1e-2 1] [7 30]} {[1e-2 1] [7 30]}
+    {[1e-2 1] [7 30]} {[1e-2 1] [7 30]}
+    {[1e-2 1] [7 30]} {[1e-2 1] [7 30]}
+    {[1e-2 1] [7 30]} {[1e-2 1] [7 30]}};
+
+%% Time ranges for elastic plateau onset
+% tRanges.d2021_07_23.c2 = {...
+%     {[1e-3 1e-2] [0.1 2]} {[1e-3 5e-2] [1 20]}
+%     {[1e-3 1e-2] [0.1 2]} {[1e-3 5e-2] [1 20]}
+%     {[1e-3 1e-2] [1 20]} {[1e-3 5e-2] [2 20]}
+%     {[1e-3 1e-2] [1 20]} {[1e-3 5e-2] [2 20]}
+%     {[1e-3 1e-2] [1 20]} {[1e-3 5e-2] [2 20]}
+%     {[1e-3 5e-2] [1 40]} {[1e-3 5e-2] [5 40]}
+%     {[1e-3 5e-2] [1 4]} {[1e-3 5e-2] [1 4]}
+%     {[1e-3 1e-1] [3 40]} {[1e-3 5e-2] [5 40]}
+%     {[1e-3 1e-1] [3 40]} {[1e-3 5e-2] [3 40]}
+%     {[1e-3 1e-1] [3 40]} {[1e-3 5e-2] [3 40]}};
+% tRanges.d2021_07_23.c1 = {...
+%     {[1e-2 1] [6 50]} {[1e-2 1] [4 20]}
+%     {[1e-2 1] [6 50]} {[1e-2 1] [4 7]}
+%     {[1e-2 1] [6 25]} {[1e-2 1] [4 7]}
+%     {[1e-2 1] [6 50]} {[1e-2 1] [4 20]}
+%     {[1e-2 1] [6 50]} {[1e-2 1] [4 20]}%5
+%     {[1e-2 1] [6 50]} {[1e-2 1] [4 20]}
+%     {[3.6 4] [5 7]} {[1e-1 1] [9 30]}%{[1e-3 1e-1] [0.3 3]} {[1e-2 1] [9 21]}
+%     {[1e-2 1] [6 50]} {[1e-2 1] [4 20]}
+%     {[0.1 0.5] [2 50]} {[1e-2 1] [4 20]}
+%     {[0.1 0.5] [4 50]} {[1e-2 1] [4 20]}};
+end
+
+
 end
