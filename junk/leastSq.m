38b405e702f0d725659b0abb7e2946de05949310
beeeel <bill.hardiman1995@gmail.com>
2021-11-17 16:30:39 +0000

    improved header comment

--- junk/leastSq.m
+++ junk/leastSq.m
@@ -1,7 +1,8 @@
 function [alpha, D] = leastSq(x, y)
-%% [alpha, D] = leastSq(x, y)
+%% [alpha, D] = leastSq(log(x), log(y))
 % Least squares estimator from Ling 2019, eq. 2.4. For regions where
 % log(MSD) is expected to be linear with log(τ).
+% Note: This gives correct D for 1D case. For 2D ÷ 2, 3D ÷ 3
 validateattributes(x,{'numeric'},{'column'})
 validateattributes(y,{'numeric'},{'column'})
 
