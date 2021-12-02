8d0188d4999aa909e275dd5c215c1278c5dc95dd
beeeel <bill.hardiman1995@gmail.com>
2021-12-02 16:18:41 +0000

    made this kinda work but not very well

--- junk/lowpass_logspace.m
+++ junk/lowpass_logspace.m
@@ -1,4 +1,15 @@
-function G = lowpass_logspace(omega, G, lpFrq)
-%% G = lowpass_logspace(omega, G, lowpassFrq)
+function G = lowpass_logspace(omega, G, lpFrq, varargin)
+%% G = lowpass_logspace(omega, G, lowpassFrq, [nInterp])
 
-omega1 = linspace
+% nInterp = 1e4;
+% if nargin > 3
+%     nInterp = varargin{1};
+% end
+    
+% omega1 = linspace(omega(1), omega(end), nInterp);
+% G1 = interp1(omega, G, omega1);
+
+% G1 = lowpass(G1, lpFrq, 1/diff(omega1([2 1])));
+G = lowpass(G, lpFrq);
+
+% G = interp1(omega1, G1, omega);
