2358dafc092593f4477ae97af84bc120a1063c40
beeeel <bill.hardiman1995@gmail.com>
2022-02-14 14:46:42 +0000

    now does B&W image with coloured scatter on top

--- junk/bead_imageAndScatter.m
+++ junk/bead_imageAndScatter.m
@@ -3,7 +3,15 @@ function bead_imageAndScatter(data)
 figure
 clf
 
-imagesc(data.ImstackFullFoV{1}{1,1})
+% To get a black and white image, first choose display range by percentile
+% and then give imagesc an [m,n,3] matrix.
+im = data.ImstackFullFoV{1}{1,1};
+lims = prctile(im, [0.1 99.9],'all');
+im = im - lims(1);
+im(im > lims(2)-lims(1)) = lims(2)-lims(1);
+im = double(im)./double(lims(2));
+
+imagesc(im.*ones(1,1,3))
 axis image
 hold on
 
@@ -11,4 +19,4 @@ roi = data.opts.roi;
 plot(roi(1) * ones(1,5) + roi(3) * [0 1 1 0 0], roi(2) * ones(1,5) + roi(4) * [0 0 1 1 0], 'r-','LineWidth',2)
 plot(data.opts.cCentre(1), data.opts.cCentre(2), 'kx','LineWidth',3)
 
-scatter(data.raw.xCentresPx(1,:)+roi(1), data.raw.yCentresPx(1,:)+roi(2), '.')
+scatter(data.raw.xCentresPx(1,:)+roi(1), data.raw.yCentresPx(1,:)+roi(2), [], data.raw.timeVecMs*1e-3, '.')
