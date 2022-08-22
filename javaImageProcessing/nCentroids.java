08d8c778072c7a692e3d42cacdc03d03bb55a656
beeeel <50520111+beeeel@users.noreply.github.com>
2022-08-22 16:44:36 +0100

    Fixed some bugs and made it work

--- javaImageProcessing/nCentroids.java
+++ javaImageProcessing/nCentroids.java
@@ -7,8 +7,8 @@ public class nCentroids{
 public static double[] getNCentroidsBasic(short[] pixels, int width, int height, int[] subWidth, short thresh) 
 {
     // Finds centroid for each object based on brightness
-    int i, j, count, nB = length(subWidth);// Number of beads is number of subwidths - last one being the fullwidth
-    double CenterOfMass[], v[], sum[], xsum[], ysum[];
+    int i, j, count[], nB = subWidth.length; // Number of beads is number of subwidths - last one being the fullwidth
+    double CentreOfMass[], v, sum[], xsum[], ysum[];
     
     count = new int[ nB ];
     CentreOfMass = new double[ 2*nB ]; // 2D CoM per particle
@@ -31,8 +31,8 @@ public static double[] getNCentroidsBasic(short[] pixels, int width, int height,
 
             // Running totals
             sum[b]  += v;
-			xsum[b] += x*v;
-			ysum[b] += y*v;
+	    xsum[b] += x*v;
+	    ysum[b] += y*v;
 			
             // Increment pixel count
 			i++;
@@ -42,10 +42,10 @@ public static double[] getNCentroidsBasic(short[] pixels, int width, int height,
 	
     for (int b=0; b < nB; b++) // Calculate CoM from running totals
     {
-        CenterOfMass[2*b]   = xsum[b]/sum[b]+0.5;
-        CenterOfMass[2*b+1] = ysum[b]/sum[b]+0.5;
+        CentreOfMass[2*b]   = xsum[b]/sum[b]+0.5;
+        CentreOfMass[2*b+1] = ysum[b]/sum[b]+0.5;
     }
-    return CenterOfMass;
+    return CentreOfMass;
 }
 
 // END OF CLASS
