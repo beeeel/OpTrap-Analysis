7879302469a0407780a3982d66f9c6f6bb4ccf3d
will hardiman <bill.hardiman1995@gmail.com>
2022-09-27 23:47:07 +0100

    reworked and bugfixed

--- beads/accu_Ddist.m
+++ beads/accu_Ddist.m
@@ -1,15 +1,26 @@
-function [z, rho, ngp, N] = accu_Ddist(accumulated, normT)
-%% [z, rho, ngp] = accu_Ddist(accumulated, [normT])
+function [z, rho, ngp, N] = accu_Ddist(accumulated, varargin)
+%% [z, rho, ngp] = accu_Ddist(accumulated, [...])
 % Calculate Ddist for all data in accumulated and return the normalised
 % increments, z, the p.d.f., rho, and the non-gaussian parameter α with lag
 % times, ngp.
+% 
+% Additional options as name-value pairs. Possibilities:
+%  normT        Normalise time domain before calculating increments
+%  posiT        Which times in accumulated to use (-1 for -ve, 0 for all, +1 for +ve)
 
-if ~exist('normT', 'var')
-    normT = false;
-end
+p = inputParser;
+
+addRequired(p, 'accumulated', @(x) isa(x, 'cell'));
+addOptional(p, 'normT', false, @(x) islogical(x) );
+addOptional(p, 'posiT', 0, @(x) x == -1 || x == 0 || x == 1);
+
+parse(p, accumulated, varargin{:});
+
+normT = p.Results.normT;
+posiT = p.Results.posiT;
 
 % All scenarios
-dt = [1; 2; 4; 6] .* logspace(-4, 3, 8);
+dt = [1; 2; 4; 6] .* logspace(-4, 4, 9);
 dt = dt(:)';
 % This is a guess at the size we'll need
 rho = zeros(140, length(dt), 2);
@@ -19,53 +30,69 @@ N = zeros(length(dt),2);
 for dIdx = 1:size(accumulated,2)
     for cIdx = 1:size(accumulated{1,dIdx},2)
         for rIdx = 1:size(accumulated{1,dIdx}{1,cIdx},2)
-            % Get the track and calculate the Ddist in a fresh msdanalyzer
-            t = accumulated{1,dIdx}{1,cIdx}(rIdx).msd.tracks;
-            m = msdanalyzer(1, 'um', 's', 'log');
-            m = m.addAll(t);
-            m = m.computeDdist;
-            
-            % Janky but it should work
-            if normT
-                tnorm = accumulated{1,dIdx}{1,cIdx}(rIdx).tnorm;
+            obsT = accumulated{1,dIdx}{2,cIdx}(rIdx);
+            if ~posiT || sign(obsT) == posiT
+                % Get the track and calculate the Ddist in a fresh msdanalyzer
+                t = accumulated{1,dIdx}{1,cIdx}(rIdx).msd.tracks;
+                m = msdanalyzer(1, 'um', 's', 'log');
+                m = m.addAll(t);
+                m = m.computeDdist;
+                
+                % Janky but it might now work
+                if normT
+                    tnorm = accumulated{1,dIdx}{1,cIdx}(rIdx).tnorm;
+                else
+                    tnorm = [1 1];
+                end
+                
                 dtnorm = round(m.Ddist{1,1}(:,1),1, 'significant')...
                     ./tnorm;
                 [tErr, tInd] = min(abs(dt - dtnorm(1,:)'), [], 2);
                 [~, ind] = max(tErr);
                 fprintf('tErr = %gs, or %g%% \n', tErr(ind), tErr(ind)./dtnorm(1,ind))
-            else
-                % "should work"
-                tInd = [1 1];
-            end
-
-            
-            % Look I'm really sorry about this, I just couldn't think of a
-            % better way to make it robust to unexpected size of Ddist.
-            ddsz = size(m.Ddist{1,2}(1:floor(end/2),:))+[0, max(tInd)];
-            larger = ddsz > size(rho(:,:,1));
-            if any(larger)
-                warning('Resizing rho to [%i %i]', ddsz(1), ddsz(2))
-            end
-            if larger(1)
-                rho(ddsz(1),1,1) = 0;
-            end
-            if larger(2)
-                rho(1,ddsz(2),1) = 0;
-                N(1,ddsz(2)) = 0;
-            end
-        
-            % Add the counts to the total
-            for dim = 1:2
-                ind = size(m.Ddist{dim,2},2);
-                N(tInd(dim)+(1:ind),dim) = N(tInd(dim)+(1:ind),dim) + m.Ddist{1}(:,2);
-                rho(:,tInd(dim)+(1:ind),dim) = rho(:,tInd(dim)+(1:ind),dim) + m.Ddist{dim,2}(1:floor(end/2),1:ind);
+                
+                % Look I'm really sorry about this, I just couldn't think of a
+                % better way to make it robust to unexpected size of Ddist.
+                ddsz = size(m.Ddist{1,2}(1:floor(end/2),:))+[0, max(tInd)];
+                larger = ddsz > size(rho(:,:,1));
+                if any(larger)
+                    warning('Resizing rho to [%i %i]', ddsz(1), ddsz(2))
+                end
+                if larger(1)
+                    rho(ddsz(1),1,1) = 0;
+                end
+                if larger(2)
+                    rho(1,ddsz(2),1) = 0;
+                    N(ddsz(2), 1) = 0;
+                end
+                
+                try 
+                    % Add the counts to the total
+                    for dim = 1:2
+                        ind = size(m.Ddist{dim,2},2);
+                        N(tInd(dim)+(1:ind),dim) = N(tInd(dim)+(1:ind),dim) + m.Ddist{1}(:,2);
+                        rho(:,tInd(dim)+(1:ind),dim) = rho(:,tInd(dim)+(1:ind),dim) + m.Ddist{dim,2}(1:floor(end/2),1:ind);
+                    end
+%                     disp('dt and N this time:')
+%                     str = repmat('\n%.1g\t\t%g\t\t%g\n',1,size(N,1));
+%                     a = zeros(2,size(N,1));
+%                     a(:,tInd(dim)+(1:ind)) = round(m.Ddist{1}(:,1:2)',1,'significant');
+%                     a = [a; N(:,1)'];
+%                     fprintf(str, a) 
+%                     fprintf('dt\t\tN now \t\t N total\n\n')
+%                     if N(19,1)==0
+%                         error('ya')
+%                     end
+                catch ME 
+                    error(ME.message, 'uhhh')
+                end
             end
         end
     end
 end
 
 % Normalise and prepare for output
-rho = rho ./ sum(rho);
+rho = rho ./ reshape(N,1,[],2);
 z = m.Ddist{1,2}(ceil(end/2):end,1);
 z = z(1:end-1) + diff(z(1:2))/2;
 
