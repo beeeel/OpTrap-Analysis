c8d95ab91cc20ff7597ecd91311c6d735133919b
beeeel <bill.hardiman1995@gmail.com>
2024-02-06 11:10:35 +0000

    can provide perpendicular or parallel translational viscosity correction

--- helpers/faxens_law.m
+++ helpers/faxens_law.m
@@ -1,6 +1,10 @@
-function correction = faxens_law(a, h)
-%% correction = faxens_law(a, h)
-% Calculate viscosity correction based on Faxen's law
-% correction = 1 / (1 - 9 / 16 * a / h + 1 / 8 * (a / h)..^3 - 45 / 256 * (a / h).^3 - 1 / 16 * (a / h).^5);
+function correction = faxens_law(a, h, parallel)
+%% correction = faxens_law(a, h, [parallel])
+% Calculate viscosity correction based on Faxen's law, default to parallel
+% correction = 1 / (1 - 9 / 16 * a / h + 1 / 8 * (a / h)^3 - 45 / 256 * (a / h).^4 - 1 / 16 * (a / h).^5);
 
-correction = 1 ./ (1 - 9 / 16 * a ./ h + 1 / 8 * (a ./ h).^3 - 45 / 256 * (a ./ h).^3 - 1 / 16 * (a ./ h).^5);
+if ~exist('parallel','var') || parallel
+    correction = 1 ./ (1 - 9 / 16 * a ./ h + 1 / 8 * (a ./ h).^3 - 45 / 256 * (a ./ h).^4 - 1 / 16 * (a ./ h).^5);
+else
+    correction = 1 ./ (1 - 9 / 8 * a ./ h + 1 / 2 * (a ./ h).^3 );
+end
