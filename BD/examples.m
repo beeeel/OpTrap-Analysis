a9bc39a46f6fa7533a153c74b959c9ce6cb07f91
will hardiman <bill.hardiman1995@gmail.com>
2024-06-17 16:39:24 +0100

    added another example with realistic amplitude double exponential force relaxation

--- BD/examples.m
+++ BD/examples.m
@@ -97,7 +97,7 @@ data = bead_ACF(data);
 % Oscillation ampltude in terms of the ratio of thermal to electrical forces
 gamma = data.pro.acfFit(1).fo.gamma; 
 
-%% Example 3: Double exponential relaxation
+%% Example 3: Double exponential relaxation (extreme amplitude version)
 
 % 0) Define an anonymous function for the force. The double exponential 
 
@@ -109,6 +109,59 @@ A0 = 1e4;   % Total amplitude (i.e. value at t=tau)
 A1 = 7000;   % Amplitude of component 1
 T1 = 0.25;  % Relaxation time of first component
 T2 = 10;    % Relaxation time of second component
+% Then we declare the function by starting with an '@' and the input
+% arguments in parenthesis. 
+E_func = @(x, y, z, t) [1, 1, 0] .* (reshape(t - tau,1,1,[]) >= 0) .* (A1 * exp(-reshape(t - tau,1,1,[]) ./ T1) ...
+    + (A0 - A1) * exp(-reshape(t - tau,1,1,[]) ./ T2));
+% The '[1, 1, 0]' means the force is in X and Y directions
+
+% We can then use the anonymous function like a regular function
+t = linspace(0,10,10e3); % Time array for input
+E = E_func(0,0,0,t);   % The output will have size [1 x 3 x T] where T is the number of time points
+
+figure
+semilogy(t,squeeze(E(1,1,:)))
+xlabel('Time (s)')
+ylabel('E (V/m)')
+
+% 1) Opts struct. This time we're simulating for 2 beads, set by the first
+% input argument. We're providing an electric field and a valence for the
+% bead (i.e. the number for q_bead is in units of proton charges)
+opts = func_BDopts(1, 'radius',3e-6,'kappaNm',[1 1 0.2] * 1e-6, ...
+    'E_func', E_func, 'q_bead', 1e3, ...
+    'Nt', 5e5, 'eta', 0.87e-3, 'temp', 20, 'output','data');
+
+% 2) Run the simulation. The multiple beads data all goes in one struct as
+% different rows in the centres matrices (e.g. data.pro.xCentresM will have
+% 2 rows and 500,000 columns)
+data = func_simulateDataField(opts);
+
+% 3) Look at the data:
+
+% Position-time data
+bead_plotProData(data);
+
+% Now imagine you're going to do some more analysis elsewhere. To write the
+% X position to a csv file, you can use this, but be careful because the
+% file sizes can be very large due to the high sampling rate and low
+% efficiency of the csv format. 
+% % dlmwrite('example_X_position_data.csv', data.pro.xCentresM(1,:)','precision','%.9g');
+% Note: Always use dlmwrite instead of csvwrite because csvwrite is limited
+% to 5 significant figures, here I am writing 9 significant figures,
+% defined by the precision argument.
+
+%% Example 4: Double exponential relaxation (realistic amplitude version)
+
+% 0) Define an anonymous function for the force. The double exponential 
+
+% First we need to define the constants used in the function, two
+% amplitudes and two rates, and one time offset (relaxation doesn't start
+% at time 0).
+tau = 1;    % Time (in seconds) to turn 'on' field
+A0 = 2e3;   % Total amplitude (i.e. value at t=tau). This value seems about right
+A1 = 0.93*A0;   % Amplitude of component 1
+T1 = 6.4e-1;  % Relaxation time of first component
+T2 = 0.36;    % Relaxation time of second component
 % Then we declare the function by starting with an '@' and the input
 % arguments in parenthesis. 
 E_func = @(x, y, z, t) [1, 1, 0] .* (reshape(t - tau,1,1,[]) >= 0) .* (A1 * exp(-reshape(t - tau,1,1,[]) ./ T1) ...
